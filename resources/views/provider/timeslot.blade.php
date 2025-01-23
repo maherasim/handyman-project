3fb452f77c0b7c977f7c13c7d0e0217b23ea921b
Asim Riaz <asimriazasim107@gmail.com>
2025-01-23 21:36:03 +0500

    calender desing update

--- resources/views/provider/timeslot.blade.php
+++ resources/views/provider/timeslot.blade.php
@@ -22,10 +22,51 @@
                                 {{ html()->hidden('id', $provider_id)->class('form-control')->attribute('placeholder', 'id') }}
                                 <div class="form-group has-feedback">
                                     <a class="me-2 float-end btn btn-sm btn-primary" href="{{ route('provider.edit-time-slot', ['id' => $provider_id]) }}" title="{{ __('messages.slot') }}">{{ __('messages.update') }}</a>
+                                    {{ html()->label(__('messages.day') . ' <span class="text-danger">*</span>', 'Day')->class('form-control-label col-md-12') }}
+                                    
+                                    <!-- Day Calendar Design -->
+                                    <div class="col-md-12">
+                                        <div class="d-flex flex-wrap gap-2 mt-3">
+                                            @foreach ($slotsArray as $slotDay)
+                                                @if (isset($slotDay['day']))
+                                                    <button type="button" 
+                                                            class="btn btn-outline-primary day-link" 
+                                                            data-day="{{ $slotDay['day'] }}" 
+                                                            id="btn-{{ $slotDay['day'] }}">
+                                                        {{ ucfirst($slotDay['day']) }}
+                                                    </button>
+                                                @endif
+                                            @endforeach
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="form-group has-feedback">
-                                    <!-- Calendar Container -->
-                                    <div id="calendar"></div>
+
+                                <div class="form-group has-feedback mt-4">
+                                    <div class="col-md-12">
+                                        {{ html()->label(__('messages.time') . ' <span class="text-danger">*</span>', 'Time')->class('form-control-label col-md-12') }}
+                                        <div class="tab-content" id="pills-tabContent-1">
+                                            @foreach ($slotsArray as $slotDay)
+                                                @if (isset($slotDay['day']) && isset($slotDay['slot']))
+                                                    <div class="tab-pane p-1 day-slot @if (strtolower($slotDay['day']) === strtolower($activeDay)) active @endif" id="{{ $slotDay['day'] }}">
+                                                        @if ($slotDay['slot'])
+                                                            <div class="d-flex flex-wrap gap-2">
+                                                                @foreach ($slotDay['slot'] as $slot)
+                                                                    @php
+                                                                        $slot = sprintf('%02d:00', $slot);
+                                                                    @endphp
+                                                                    <span class="badge bg-primary p-2">{{ $slot }}</span>
+                                                                @endforeach
+                                                            </div>
+                                                        @else
+                                                            <div>
+                                                                <span>No time slots selected for {{ $slotDay['day'] }} day.</span>
+                                                            </div>
+                                                        @endif
+                                                    </div>
+                                                @endif
+                                            @endforeach
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -36,46 +77,31 @@
         </div>
     </div>
 
-    @section('bottom_script')
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
     <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            var calendarEl = document.getElementById('calendar');
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                plugins: ['dayGrid', 'timeGrid', 'list', 'interaction', 'bootstrap'],
-                themeSystem: 'bootstrap', // Use Bootstrap styling
-                header: {
-                    left: 'prev,next today', // Navigation buttons
-                    center: 'title',        // Calendar title
-                    right: 'dayGridMonth,timeGridWeek,timeGridDay' // View options
-                },
-                height: 600, // Set calendar height
-                selectable: true, // Enable date selection
-                editable: false, // Disable dragging events
-                eventLimit: false, // Do not limit event display
-                events: [
-                    @foreach ($slotsArray as $slotDay)
-                        @if (isset($slotDay['slot']))
-                            @foreach ($slotDay['slot'] as $slot)
-                                {
-                                    title: "{{ ucfirst($slotDay['day']) }} Slot",
-                                    start: "{{ $slotDay['day'] . 'T' . sprintf('%02d:00', $slot) }}", // Date and time
-                                    color: 'rgb(19, 193, 240)', // Custom event color
-                                    textColor: '#fff' // Event text color
-                                },
-                            @endforeach
-                        @endif
-                    @endforeach
-                ],
-                eventClick: function (info) {
-                    // Redirect to the booking details page on event click
-                    var id = info.event.id;
-                    var url = "{{ URL::to('booking') }}/" + id;
-                    window.location.replace(url);
-                },
+        $(document).ready(function () {
+            setActiveDay('mon'); // Default active day
+
+            $('.day-link').on('click', function (e) {
+                e.preventDefault();
+                var selectedDay = $(this).data('day');
+                setActiveDay(selectedDay);
+                showActiveDaySlots();
             });
 
-            calendar.render();
+            function setActiveDay(day) {
+                $('.day-slot').removeClass('active');
+                $('.day-link').removeClass('btn-primary').addClass('btn-outline-primary');
+                $('#btn-' + day).removeClass('btn-outline-primary').addClass('btn-primary');
+                $('.day-slot#' + day).addClass('active');
+                activeDay = day;
+            }
+
+            function showActiveDaySlots() {
+                $('.day-slot').hide();
+                $('.day-slot.active').show();
+            }
         });
     </script>
-    @endsection
 </x-master-layout>
