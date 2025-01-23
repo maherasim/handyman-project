5cabc0b68fad746a7dbd1498bd6743e2e7288961
Asim Riaz <asimriazasim107@gmail.com>
2025-01-23 21:39:35 +0500

    calender desing update

--- resources/views/provider/timeslot.blade.php
+++ resources/views/provider/timeslot.blade.php
@@ -22,6 +22,105 @@
                                 {{ html()->hidden('id', $provider_id)->class('form-control')->attribute('placeholder', 'id') }}
                                 <div class="form-group has-feedback">
                                     <a class="me-2 float-end btn btn-sm btn-primary" href="{{ route('provider.edit-time-slot', ['id' => $provider_id]) }}" title="{{ __('messages.slot') }}">{{ __('messages.update') }}</a>
+                                </div>
+                                <div class="form-group has-feedback">
+                                    <!-- Calendar Container -->
+                                    <div id="calendar"></div>
+                                </div>
+                            </div>
+                        </div>
+                        {{ html()->form()->close() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @section('bottom_script')
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                plugins: ['dayGrid', 'timeGrid', 'list', 'interaction', 'bootstrap'],
+                themeSystem: 'bootstrap', // Use Bootstrap styling
+                header: {
+                    left: 'prev,next today', // Navigation buttons
+                    center: 'title',        // Calendar title
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay' // View options
+                },
+                height: 600, // Set calendar height
+                selectable: true, // Enable date selection
+                editable: false, // Disable dragging events
+                eventLimit: false, // Do not limit event display
+                events: [
+                    @foreach ($slotsArray as $slotDay)
+                        @if (isset($slotDay['slot']))
+                            @foreach ($slotDay['slot'] as $slot)
+                                {
+                                    title: "{{ ucfirst($slotDay['day']) }} Slot",
+                                    start: "{{ $slotDay['day'] . 'T' . sprintf('%02d:00', $slot) }}", // Date and time
+                                    color: 'rgb(19, 193, 240)', // Custom event color
+                                    textColor: '#fff' // Event text color
+                                },
+                            @endforeach
+                        @endif
+                    @endforeach
+                ],
+                eventClick: function (info) {
+                    // Redirect to the booking details page on event click
+                    var id = info.event.id;
+                    var url = "{{ URL::to('booking') }}/" + id;
+                    window.location.replace(url);
+                },
+            });
+
+            calendar.render();
+        });
+    </script>
+    @endsection
+</x-master-layout>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{{-- <x-master-layout>
+    <div class="container-fluid">
+        @include('partials._provider')
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card card-block card-stretch">
+                    <div class="card-body p-0">
+                        <div class="d-flex justify-content-between align-items-center p-3 flex-wrap gap-3">
+                            <h5 class="fw-bold">
+                                {{ $providerdata->first_name . ' ' . $providerdata->last_name }} {{ $pageTitle }}
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        {{ html()->form('POST', '#')->attribute('data-toggle', 'validator')->id('provider')->open() }}
+                        <div class="row">
+                            <div class="col-md-12">
+                                {{ html()->hidden('id', $provider_id)->class('form-control')->attribute('placeholder', 'id') }}
+                                <div class="form-group has-feedback">
+                                    <a class="me-2 float-end btn btn-sm btn-primary" href="{{ route('provider.edit-time-slot', ['id' => $provider_id]) }}" title="{{ __('messages.slot') }}">{{ __('messages.update') }}</a>
                                     {{ html()->label(__('messages.day') . ' <span class="text-danger">*</span>', 'Day')->class('form-control-label col-md-12') }}
                                     
                                     <!-- Day Calendar Design -->
@@ -104,4 +203,4 @@
             }
         });
     </script>
-</x-master-layout>
+</x-master-layout> --}}
