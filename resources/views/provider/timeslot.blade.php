dbe9af1e6bb3d6090f33916cc01d2998880947a1
Asim Riaz <asimriazasim107@gmail.com>
2025-01-23 21:29:37 +0500

    calender desing update

--- resources/views/provider/timeslot.blade.php
+++ resources/views/provider/timeslot.blade.php
@@ -37,44 +37,6 @@
 </x-master-layout>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        var calendarEl = document.getElementById('calendar');
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-            plugins: ['dayGrid', 'interaction'],
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'dayGridMonth'
-            },
-            height: 600,
-            selectable: true,
-            events: [
-                // Populate events dynamically from your $slotsArray
-                @foreach ($slotsArray as $slotDay)
-                    @if (isset($slotDay['slot']))
-                        @foreach ($slotDay['slot'] as $slot)
-                            {
-                                title: '{{ ucfirst($slotDay['day']) }} Slot',
-                                start: '{{ date('Y-m-d', strtotime($slotDay['day'])) }}T{{ sprintf('%02d:00:00', $slot) }}',
-                                allDay: false
-                            },
-                        @endforeach
-                    @endif
-                @endforeach
-            ],
-            select: function (info) {
-                alert('Selected: ' + info.startStr);
-                // Handle slot selection here
-            },
-            eventClick: function (info) {
-                alert('Event: ' + info.event.title);
-                // Handle slot click here
-            }
-        });
-        calendar.render();
-    });
-</script>
 
 <script>
     $(document).ready(function () {
