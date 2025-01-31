d0fb25ef6fcd94d57717070e1eec7bc2b0666c0e
Asim Riaz <asimriazasim107@gmail.com>
2025-01-31 10:28:00 +0500

    frontedn page modification

--- resources/views/service/datatable-card.blade.php
+++ resources/views/service/datatable-card.blade.php
@@ -1,3 +1,19 @@
+<style>
+   .price-box {
+      background-color: #007bff; /* Blue background */
+      color: white; /* White text for better visibility */
+      font-size: 18px; /* Increased text size */
+      font-weight: bold;
+      color: red; /* Red text for price */
+      text-align: center;
+      padding: 10px 15px; /* Added consistent padding */
+      border-radius: 10px; /* Rounded corners */
+      display: inline-block;
+      radius: 15%;
+      width: 180px; /* Increased width */
+      margin: 5px 0; /* Optional: Adds spacing around the box */
+   }
+</style>
 <div class="service-box-card bg-light rounded-3 mb-5" data-service-id="{{ $data->id }}">
    <div class="iq-image position-relative">
       @if($data->visit_type == 'ONLINE')
@@ -19,7 +35,7 @@
                   <input type="hidden" name="user_id" id="user_id" value="{{ Auth::user()->id }}">
                @endif
                <button type="button" class="btn-link serv-whishlist text-primary save_fav">
-                  <svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns=" ">
                      <path fill-rule="evenodd" clip-rule="evenodd" d="M1.43593 6.29916C0.899433 4.62416 1.52643 2.70966 3.28493 2.14316C4.20993 1.84466 5.23093 2.02066 5.99993 2.59916C6.72743 2.03666 7.78593 1.84666 8.70993 2.14316C10.4684 2.70966 11.0994 4.62416 10.5634 6.29916C9.72843 8.95416 5.99993 10.9992 5.99993 10.9992C5.99993 10.9992 2.29893 8.98516 1.43593 6.29916Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                      <path d="M8 3.84998C8.535 4.02298 8.913 4.50048 8.9585 5.06098" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                   </svg>
@@ -34,7 +50,7 @@
                   <input type="hidden" name="user_id" id="user_id" value="{{ Auth::user()->id }}">
                @endif
                <button type="button" class="btn-link serv-whishlist text-primary delete_fav">
-                  <svg width="12" height="13" viewBox="0 0 12 13" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <svg width="12" height="13" viewBox="0 0 12 13" fill="currentColor" xmlns=" ">
                      <path fill-rule="evenodd" clip-rule="evenodd" d="M1.43593 6.29916C0.899433 4.62416 1.52643 2.70966 3.28493 2.14316C4.20993 1.84466 5.23093 2.02066 5.99993 2.59916C6.72743 2.03666 7.78593 1.84666 8.70993 2.14316C10.4684 2.70966 11.0994 4.62416 10.5634 6.29916C9.72843 8.95416 5.99993 10.9992 5.99993 10.9992C5.99993 10.9992 2.29893 8.98516 1.43593 6.29916Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                      <path d="M8 3.84998C8.535 4.02298 8.913 4.50048 8.9585 5.06098" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                   </svg>
@@ -45,7 +61,7 @@
          <form method="GET" id="favoriteForm" action="{{ route('user.login') }}">
             @csrf
             <button type="submit" class="btn-link serv-whishlist text-primary">
-               <svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+               <svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns=" ">
                   <path fill-rule="evenodd" clip-rule="evenodd" d="M1.43593 6.29916C0.899433 4.62416 1.52643 2.70966 3.28493 2.14316C4.20993 1.84466 5.23093 2.02066 5.99993 2.59916C6.72743 2.03666 7.78593 1.84666 8.70993 2.14316C10.4684 2.70966 11.0994 4.62416 10.5634 6.29916C9.72843 8.95416 5.99993 10.9992 5.99993 10.9992C5.99993 10.9992 2.29893 8.98516 1.43593 6.29916Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                   <path d="M8 3.84998C8.535 4.02298 8.913 4.50048 8.9585 5.06098" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                </svg>
@@ -53,33 +69,24 @@
          </form>
       @endif
 
-   </div>
-   <a href="{{ route('service.detail', $data->id) }}" class="service-heading mt-4 d-block p-0">
-      <h5 class="service-heading service-title font-size-18 line-count-2">{{ $data->name }}</h5>
-   </a>
-   <ul class="list-inline p-0 mt-0 mb-0 price-content">
+   </div> 
+   <ul>
    @if($data->price==0)
-      <li class="text-primary fw-500 d-inline-block position-relative font-size-18">Free</li>
-      @else
- <li class="text-primary fw-500 d-inline-block position-relative font-size-18">{{ getPriceFormat($data->price) }}</li>
-      @endif
-      @if(!empty($data->duration))
-      @php
-         $durationParts = explode(':', $data->duration);
-         $hours = intval($durationParts[0]);
-         $minutes = intval($durationParts[1]);
-      @endphp
-      <li class="d-inline-block fw-500 position-relative service-price">
-         <!-- ({{ $data->duration }} min) -->
-         @if($hours > 0)
-            ({{ $hours }} hrs @if($minutes > 0) {{ $minutes }} min @endif)
-         @else
-            ({{ $minutes }} min)
-         @endif
-      </li>
-      @endif
-   </ul>
-   <div
+   <li class="text-primary fw-500 d-inline-block position-relative font-size-18">Free</li>
+   @else
+<li class="price-box fw-500 d-inline-block position-relative font-size-18">{{ getPriceFormat($data->price) }} / {{ $data->type }}</li>
+</ul>
+   @endif 
+   <a href="{{ route('service.detail', $data->id) }}" class="service-heading mt-4 d-block p-0">
+      <h5 class="service-heading service-title font-size-18 line-count-2" style="font-family: Georgia, serif;">
+         {{ $data->name }}
+     </h5>
+        </a>
+  
+  <h5 class="font-size-18 line-count-2" > {{ $data->city ? $data->city->name : 'N/A' }}  -  {{ $data->country ? $data->country->name : 'N/A' }}</h5>
+    
+
+  <div
       class="mt-3">
       <div class="d-flex align-items-center gap-2">
          <img src="{{ getSingleMedia($data->providers,'profile_image', null) }}" alt="service" class="img-fluid rounded-3 object-cover avatar-24">
@@ -87,9 +94,21 @@
             <span class="font-size-14 service-user-name">{{ ($data->providers)->display_name }}</span>
          </a>
       </div>
+      <div class="d-flex align-items-center gap-2">
+         
+         <span class="font-size-20 line-count-2" style="font-family: Georgia, serif;">
+            <strong>{{ $completedBookingCount }} Bookings</strong>    
+        </span>
+      
+         <img src="{{ asset('images/plans/icon.jpg') }}" alt="icon"
+         style="width: 23%; height: 23%; margin-right: 10px;"> <img src="{{ asset('images/plans/icon.jpg') }}" alt="icon"
+         style="width: 23%; height: 23%">
+       
+      
+      </div>
       <div class="d-flex align-items-center gap-1 f-none mt-2">
          @if($totalRating > 0)
-         <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none"
+         <svg xmlns=" " width="12" height="12" viewBox="0 0 12 12" fill="none"
             class="service-rating">
             <path
                d="M6.58578 0.85525L7.92167 3.44562C8.02009 3.63329 8.20793 3.76362 8.42458 3.79259L11.4252 4.21427C11.6005 4.23802 11.7595 4.32723 11.8669 4.46335C11.9731 4.59773 12.0187 4.76803 11.9929 4.93543C11.9719 5.07445 11.9041 5.20304 11.8003 5.30151L9.62603 7.33523C9.467 7.47714 9.39498 7.68741 9.43339 7.89304L9.96871 10.7522C10.0257 11.0974 9.78867 11.4229 9.43339 11.4884C9.28696 11.511 9.13693 11.4872 9.0049 11.4224L6.32833 10.0768C6.12968 9.98005 5.89503 9.98005 5.69639 10.0768L3.01982 11.4224C2.69094 11.5909 2.28346 11.4762 2.10042 11.1634C2.0326 11.0389 2.0086 10.897 2.0308 10.7585L2.56612 7.89883C2.60453 7.69378 2.53191 7.48236 2.37348 7.34044L0.19921 5.30788C-0.0594455 5.06692 -0.0672472 4.67014 0.181806 4.42048C0.187207 4.41527 0.193209 4.40948 0.19921 4.40369C0.302432 4.30232 0.438061 4.23802 0.584493 4.22123L3.58514 3.79896C3.80118 3.76942 3.98902 3.64025 4.08805 3.45141L5.37592 0.85525C5.49055 0.632821 5.7282 0.494383 5.98625 0.500175H6.06667C6.29052 0.526241 6.48556 0.660046 6.58578 0.85525Z"
@@ -102,8 +121,25 @@
               <a href="{{ route('rating.all', ['service_id' => $data->id]) }}" class="text-body ms-1">({{$totalReviews }} {{__('messages.review')}})</a></h6>
             @endif
          @endif
-      </div>
-   </div>
+      </div> <br>
+      <div class="d-flex mt-8 "  style="gap: 18px; justify-content: center;">
+         <a href="#"><img
+                 src="https://cdn.pixabay.com/photo/2021/06/15/12/51/facebook-6338507_1280.png"
+                 style="width: 30px; border-radius: 8px;" alt=""></a>
+         <a href="#"><img
+                 src="https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg"
+                 style="width: 30px; border-radius: 8px;" alt=""></a>
+         <a href="#"><img
+                 src="https://cdn.pixabay.com/photo/2015/03/10/17/30/twitter-667462_640.png"
+                 style="width: 30px; border-radius: 8px;" alt=""></a>
+         <a href="#"><img
+                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRokEYt0yyh6uNDKL8uksVLlhZ35laKNQgZ9g&s"
+                 style="width: 30px; border-radius: 8px;" alt=""></a>
+     </div>
+     
+   </div>  
+   
+  
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.1/dist/sweetalert2.all.min.js"></script>
