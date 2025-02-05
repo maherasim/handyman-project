8f378619f0dd25832fa9b058d66023236b7666e0
Asim Riaz <asimriazasim107@gmail.com>
2025-02-05 10:55:01 +0500

    service page issue fixed

--- resources/views/landing-page/ServiceDetail.blade.php
+++ resources/views/landing-page/ServiceDetail.blade.php
@@ -66,7 +66,7 @@
                         <div class="mt-5 pt-lg-5 pt-3">
                             <h5 class="mb-3">{{ __('Minimum Booking') }}</h5>
                             <p class="m-0">
-                                {{ $serviceData['service_detail']['minimum_booking'] }}
+                                {{-- {{ $serviceData['service_detail']['minimum_booking'] }} --}}
                             </p>
                         </div>
                     @endif
