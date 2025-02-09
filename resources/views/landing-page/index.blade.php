b3287ae55ba69a6e89cc7d174399e9218c9e97d9
Asim Riaz <asimriazasim107@gmail.com>
2025-02-09 14:17:24 +0500

    job request design modifications

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -198,7 +198,7 @@
                                         style="border-radius: 10px; width: 100%; height: 200px; object-fit: cover;">
                                     <!-- Price Overlay -->
                                     <div
-                                        style="position: absolute; bottom: -20px; left: 50%; transform: translateX(-50%); width:40%; background-image: url('{{ asset('images/icon/bannerbg.jpg') }}'); background-size: cover; background-position: center; padding: 10px 20px; color: #fff; font-weight: 600; font-size: 18px; border-radius: 10px; border: 3px solid #E1DCDD;">
+                                        style="position: absolute; bottom: -20px; left: 50%; transform: translateX(-50%); width:40%; background-image: url('{{ asset('images/icon/bannerbg.jpg') }}'); background-size: cover; background-repeat: no-repeat; background-position: center; padding: 10px 20px; color: #fff; font-weight: 600; font-size: 18px; border-radius: 10px; border: 3px solid #E1DCDD;">
                                         € {{ $jobRequest->price }} / Fix
                                     </div>
                                     <!-- Heart Icon -->
