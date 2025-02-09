40c77904f530967146f7dace1302f5a0fadbe6e5
Asim Riaz <asimriazasim107@gmail.com>
2025-02-09 14:22:09 +0500

    job request design modifications

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -229,7 +229,7 @@
                                     </div>
                                     <!-- Location -->
                                     <h5 class="mt-0 mb-1 text-truncate">
-                                        <span style="font-style: 14px;">
+                                        <span style="font-size: 14px;">
                                             @if ($jobRequest->city && $jobRequest->country)
                                                 {{ $jobRequest->city->name }} - {{ $jobRequest->country->name }}
                                             @else
