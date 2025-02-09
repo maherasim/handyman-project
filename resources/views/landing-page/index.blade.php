47508a54cbffac7d13f5edd79ce808d358fe6360
Asim Riaz <asimriazasim107@gmail.com>
2025-02-09 15:11:35 +0500

    job request design modifications

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -13,6 +13,9 @@
         .heading_text {
             font-weight: 600;
         }
+        .service{
+            margin-top: -30px;
+        }
     </style>
     <!-- Banner -->
     <div class="padding-top-bottom-90 bg-light">
@@ -383,8 +386,8 @@
                                                 </form>
                                             @endif
                                         </div>
-                                        <ul class="list-inline p-0 mt-0 mb-0 price-content">
-                                         <div class="d-flex justify-content-center " style="position:relative; z-index:1111; margin:auto; background-image: url('{{ asset('images/icon/bannerbg.jpg') }}'); background-size: cover; width:68% ; margin-top:-32px;  background-repeat: no-repeat; background-position: center; padding: 10px 20px; color: #fff; font-weight: 600; font-size: 18px; border-radius: 10px; border: 3px solid #E1DCDD;">
+                                        <ul class=" list-inline p-0 mt-0 mb-0 price-content">
+                                         <div class="service d-flex justify-content-center " style="position:relative; z-index:1111; margin:auto; background-image: url('{{ asset('images/icon/bannerbg.jpg') }}'); background-size: cover; width:68% ; margin-top:-32px;  background-repeat: no-repeat; background-position: center; padding: 10px 20px; color: #fff; font-weight: 600; font-size: 18px; border-radius: 10px; border: 3px solid #E1DCDD;">
                                             @if ($data->price == 0)
                                                 <li
                                                     class="text-primary fw-500 d-inline-block position-relative font-size-18">
