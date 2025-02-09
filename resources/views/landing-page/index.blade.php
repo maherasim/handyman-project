3e0fcad6635e97db909b66ec00ee9bb8249b0ae9
Asim Riaz <asimriazasim107@gmail.com>
2025-02-09 16:00:20 +0500

    job request design modifications

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -312,7 +312,7 @@
                         <div class="row">
                             @foreach ($servicerequest as $data)
                                 <div class="col-md-3"> <!-- Changed from col-md-4 to col-md-3 -->
-                                    <div class="service-box-card bg-light rounded-3 mb-0"
+                                    <div class="service-box-card bg-light rounded-3 mb-3"
                                         data-service-id="{{ $data->id }}">
                                         <div class="iq-image position-relative">
                                             @if ($data->visit_type == 'ONLINE')
