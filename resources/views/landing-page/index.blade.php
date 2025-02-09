4d9219c639d8c9f12d97226ce19e1a18a1c196e4
Asim Riaz <asimriazasim107@gmail.com>
2025-02-09 22:54:17 +0500

    job request design modifications

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -251,7 +251,7 @@
                                             </span>
                                         </h5>
                                         <!-- Published Info -->
-                                        <h5 class="mb-0" style="font-weight: 200;">Published at:
+                                        <h5 class="mb-0" style="font-weight: 100;">Published at:
                                             {{ $jobRequest->created_at->toDateString() }}</h5>
 
 
@@ -262,7 +262,7 @@
                                                 {{ $jobRequest->provider->username ?? 'Unknown' }}</p>
                                         </div>
                                         <!-- Status -->
-                                        <h5 style="font-weight: 200;">Status: <span
+                                        <h5 style="font-weight: 100;">Status: <span
                                                 style="font-weight: 500; font-size: 17px;">{{ $jobRequest->status }}</span>
                                         </h5>
                                     </div>
