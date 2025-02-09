9c028547788579185ce524986fa19024a4bcf5bb
Asim Riaz <asimriazasim107@gmail.com>
2025-02-09 18:20:41 +0500

    service changes

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -643,24 +643,14 @@
                                         </div>
                                     </div>
                                     <div class="d-flex align-items-center  mt-2">
-                                        @if ($data->avg_rating > 0)
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12"
-                                                viewBox="0 0 12 12" fill="none" class="service-rating">
-                                                <path d="M6.58578 0.85525L7.92167..." fill="currentColor" />
-                                            </svg>
-                                            <h6 class="font-size-14 mb-0">
-                                                {{ $data->avg_rating }}
-                                                <a href="{{ route('rating.all', ['service_id' => $data->id]) }}"
-                                                    class="text-body ms-1">
-                                                    ({{ $data->total_reviews }}
-                                                    {{ $data->total_reviews > 1 ? __('messages.reviews') : __('messages.review') }})
-                                                </a>
-
-                                            </h6>
-                                        @else
-                                            <h6 class="font-size-14 mb-0 text-muted">
-                                                {{ __('no_reviews_yet') }}</h6>
-                                        @endif
+                                        <div class="d-flex align-items-center gap-2 flex-wrap ml-2">
+                                            <div class="star-rating">
+                                                <rating-component :readonly="true" :showrating="false" :ratingvalue="1" />
+                                            </div>
+                                            <h6 class="lh-sm">1</h6>
+                                            <a href="#">(1 {{ __('messages.reviews') }})</a>
+                                        </div>
+                                        
                                         <strong>{{ $completedBookingCount }} Bookings</strong>
                                     </div>
 
