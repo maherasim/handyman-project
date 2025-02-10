e65f33b0675aeae46268f6b9b3534d572949ae88
Asim Riaz <asimriazasim107@gmail.com>
2025-02-10 15:48:32 +0500

    added stars and height reduced

--- resources/views/service/datatable-card.blade.php
+++ resources/views/service/datatable-card.blade.php
@@ -104,7 +104,7 @@
    <a href="{{ route('service.detail', $data->id) }}"
       class="service-heading mt-2 d-block p-0">
       <h5 class="service-heading service-title font-size-1 line-count-2"
-          style="font-size:13px">
+          style="font-size:12px">
           <h5><b>{{ Str::words($data->name, 3, '') }}</b></h5>
          </h5>
   </a>
