31e5dc336e101af0fcd21e32392559e8d1f2b606
Asim Riaz <asimriazasim107@gmail.com>
2025-02-10 09:05:35 +0500

    added job requests changes

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -420,24 +420,24 @@
                                         </p>
 
 
-                                        <div class="d-flex align-items-center justify-content-between"
-                                            style="width: 100%">
-                                            <div class="d-flex align-items-center">
+                                        <div class="d-flex align-items-center justify-content-between w-100">
+                                            <div class="d-flex align-items-center flex-nowrap">
                                                 <img src="{{ getSingleMedia($data->providers, 'profile_image', null) }}"
                                                     alt="service" class="img-fluid rounded-3 object-cover avatar-24">
-                                                <a href="{{ route('provider.detail', $data->providers->id) }}">
-                                                    <span
-                                                        class="font-size-14 service-user-name">{{ $data->providers->display_name }}</span>
+                                                <a href="{{ route('provider.detail', $data->providers->id) }}" class="ml-2">
+                                                    <span class="font-size-14 service-user-name" style="white-space: nowrap;">
+                                                        {{ $data->providers->display_name }}
+                                                    </span>
                                                 </a>
                                             </div>
-                                            <div class="d-flex align-items-center  mt-2 justify-content-end">
+                                            <div class="d-flex align-items-center justify-content-end">
                                                 <img src="{{ asset('images/icon/freeicon.jpg') }}" alt="icon"
-                                                    style="width: 20%; height: 70%; margin-right: 10px;"> <img
-                                                    src="{{ asset('images/icon/verified.jpg') }}" alt="icon"
-                                                    style="width: 20%; height: 8%">
-
+                                                    style="width: 20px; height: 20px; margin-right: 10px;">
+                                                <img src="{{ asset('images/icon/verified.jpg') }}" alt="icon"
+                                                    style="width: 20px; height: 20px;">
                                             </div>
                                         </div>
+                                        
                                         <div class="d-flex align-items-center mt-2 gap-3"> 
                                             <div class="d-flex align-items-center gap-2 flex-wrap ml-2">
                                                 <div class="star-rating">
