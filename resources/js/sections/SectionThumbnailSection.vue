34e5857542fb29f38e48eac8a89b1763b66f0d69
Asim Riaz <asimriazasim107@gmail.com>
2025-02-17 00:49:24 +0500

    section thumbnail issue

--- resources/js/sections/SectionThumbnailSection.vue
+++ resources/js/sections/SectionThumbnailSection.vue
@@ -1,81 +1,39 @@
 <template>
     <div class="tab-slider">
-        <!-- Main Swiper -->
-        <Swiper class="swiper-content position-relative overflow-hidden" :modules="[Controller]" :slidesPerView="1"
-            :grabCursor="true" @swiper="setMainSwiper">
+        <Swiper class="swiper-content position-relative overflow-hidden" :modules="[Controller]" direction="horizontal" :loop-slide="5" :mousewheel="true"
+            :grabCursor="true" @swiper="setThumbSwiper" :controller="{ control: mainSwiper }">
             <SwiperSlide v-for="(data, index) in props.attachments" :key="index">
-                <div>
-                    <img :src="data" alt="" loading="lazy" class="img-fluid object-cover rounded-3" />
+                <div class="">
+                    <img :src="data" alt="" loading="lazy" class="img-fluid object-cover rounded-3"/>
                 </div>
             </SwiperSlide>
         </Swiper>
     </div>
-
-    <div class="tab-slider mt-3">
-        <!-- Thumbnail Swiper -->
-        <Swiper class="swiper-thumb overflow-hidden" :modules="[Controller]" :slidesPerView="4" :spaceBetween="10"
-            :watchSlidesProgress="true" :grabCursor="true" :slideToClickedSlide="true" @swiper="setThumbSwiper"
-            @click="updateMainSwiper">
-            <SwiperSlide v-for="(data, index) in props.attachments" :key="index">
-                <div class="thumb-wrapper p-1 rounded-3">
-                    <img :src="data" alt="" loading="lazy" class="img-fluid object-cover rounded-3"
-                        @click="updateMainSwiper(index)" />
-                </div>
-            </SwiperSlide>
-        </Swiper>
-    </div>
+    <div class="tab-slider">
+    <Swiper class="swiper-thumb overflow-hidden mt-3" :modules="[Controller]" direction="horizontal" :slide-to-clicked-slide="true" :spaceBetween="3 "  @swiper="setMainSwiper"
+        :controller="{ control: thumbSwiper }">
+        <SwiperSlide v-for="data in props.attachments" :key="data">
+            <div class="thumb-wrapper p-1 rounded-3">
+                <img :src="data" alt="" loading="lazy" class="img-fluid object-cover rounded-3"  />
+            </div>
+        </SwiperSlide>
+    </Swiper>
+</div>
 </template>
-
 <script setup>
-import { ref, nextTick } from "vue";
-import { Swiper, SwiperSlide } from "swiper/vue";
-import { Controller } from "swiper";
+import { ref } from 'vue';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Controller } from 'swiper';
 
-// Define props for the component
-const props = defineProps(["attachments"]);
+const props = defineProps(['attachments']);
 
-// Main Swiper and Thumbnail Swiper references
-const mainSwiper = ref(null);
-const thumbSwiper = ref(null);
-
-// Functions to set the Swiper instances
+// Export component options
+const mainSwiper = ref(null)
+const thumbSwiper = ref(null)
 const setMainSwiper = (swiper) => {
-    mainSwiper.value = swiper;
-};
+    mainSwiper.value = swiper
+}
 const setThumbSwiper = (swiper) => {
-    thumbSwiper.value = swiper;
-};
-
-// Function to update main Swiper when a thumbnail is clicked
-const updateMainSwiper = (index) => {
-    if (mainSwiper.value) {
-        nextTick(() => {
-            mainSwiper.value.slideTo(index); // Move the main Swiper to the clicked index
-        });
-    }
-};
+    thumbSwiper.value = swiper
+}
 </script>
-
-<style scoped>
-.tab-slider .swiper-content img,
-.tab-slider .swiper-thumb img {
-    cursor: pointer;
-}
-
-.tab-slider .thumb-wrapper {
-    border: 1px solid #ddd;
-    transition: border-color 0.3s;
-}
-
-.tab-slider .thumb-wrapper:hover {
-    border-color: #000;
-}
-
-.tab-slider .swiper-thumb {
-    max-width: 100%;
-}
-
-.tab-slider .swiper-content {
-    max-width: 100%;
-}
-</style>
