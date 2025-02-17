edc17d78bb6ae334e49c92eeb865baf5782a38b0
Asim Riaz <asimriazasim107@gmail.com>
2025-02-17 21:05:57 +0500

    handyman bug fixing

--- resources/views/post-job-request/create.blade.php
+++ resources/views/post-job-request/create.blade.php
@@ -1,4 +1,6 @@
 <x-master-layout>
+    <script src="https://cdn.tiny.cloud/1/m5d82gd2rwdlg96hsxpx0e5wwmfrl2zzkcw35ys8o3glilgq/tinymce/5/tinymce.min.js"
+    referrerpolicy="origin"></script>
     <div class="container-fluid">
        <div class="row">
           <div class="col-lg-12">
@@ -146,6 +148,14 @@
               }
           });
        });
+    </script>
+     <script>
+        tinymce.init({
+            selector: '#description', // Target the ID of your textarea
+            plugins: 'lists link image preview', // Add any plugins you want to use
+            toolbar: 'undo redo | bold italic | bullist numlist | link image preview',
+            menubar: false
+        });
     </script>
     @endsection
  </x-master-layout>
