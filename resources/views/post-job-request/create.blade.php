d80175fb5494b95ce0e3dcc234c7631f73706be0
Asim Riaz <asimriazasim107@gmail.com>
2025-02-17 21:17:03 +0500

    handyman bug fixing

--- resources/views/post-job-request/create.blade.php
+++ resources/views/post-job-request/create.blade.php
@@ -163,90 +163,91 @@
     </script>
      <script>
         tinymce.init({
-            selector: '#description', // Target the ID of your textarea
-            plugins: 'lists link image preview', // Add any plugins you want to use
+            selector: '#description',
+            plugins: 'lists link image preview',
             toolbar: 'undo redo | bold italic | bullist numlist | link image preview',
             menubar: false
         });
+    
         $(document).ready(function() {
-                  var provider_id = "{{ isset($servicedata->provider_id) ? $servicedata->provider_id : '' }}";
-                  var provider_address_id = "{{ isset($data) ? $data : [] }}";
-
-                  var category_id = "{{ isset($servicedata->category_id) ? $servicedata->category_id : '' }}";
-                  var subcategory_id =
-                      "{{ isset($servicedata->subcategory_id) ? $servicedata->subcategory_id : '' }}";
-
-                  var country_id = "{{ isset($servicedata->country_id) ? $servicedata->country_id : 0 }}";
-                  var city_id = "{{ isset($servicedata->city_id) ? $servicedata->city_id : 0 }}";
-                  var price_type = "{{ isset($servicedata->type) ? $servicedata->type : '' }}";
-
-                  providerAddress(provider_id, provider_address_id)
-                  getSubCategory(category_id, subcategory_id)
-                  priceformat(price_type)
-
-                  $(document).on('change', '#provider_id', function() {
-                      var provider_id = $(this).val();
-                      $('#provider_address_id').empty();
-                      providerAddress(provider_id, provider_address_id);
-                  })
-                  $(document).on('change', '#category_id', function() {
-                      var category_id = $(this).val();
-                      $('#subcategory_id').empty();
-                      getSubCategory(category_id, subcategory_id);
-                  })
-                  $(document).on('change', '#price_type', function() {
-                      var price_type = $(this).val();
-                      priceformat(price_type);
-                  })
-
-                  $(document).on('change', '#country_id', function() {
-                      var country = $(this).val();
-                      $('#city_id').empty();
-                      cityName(country);
-                  })
-
-                  $(document).on('change', '#city_id', function() {
-                      var city = $(this).val();
-                      console.log('selected city', city);
-                  })
-
-                  $('.galary').each(function(index, value) {
-                      let galleryClass = $(value).attr('data-gallery');
-                      $(galleryClass).magnificPopup({
-                          delegate: 'a#attachment_files',
-                          type: 'image',
-                          gallery: {
-                              enabled: true,
-                              navigateByImgClick: true,
-                              preload: [0,
-                                  1
-                              ] // Will preload 0 - before current, and 1 after the current image
-                          },
-                          callbacks: {
-                              elementParse: function(item) {
-                                  if (item.el[0].className.includes('video')) {
-                                      item.type = 'iframe',
-                                          item.iframe = {
-                                              markup: '<div class="mfp-iframe-scaler">' +
-                                                  '<div class="mfp-close"></div>' +
-                                                  '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
-                                                  '<div class="mfp-title">Some caption</div>' +
-                                                  '</div>'
-                                          }
-                                  } else {
-                                      item.type = 'image',
-                                          item.tLoading = 'Loading image #%curr%...',
-                                          item.mainClass = 'mfp-img-mobile',
-                                          item.image = {
-                                              tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-                                          }
-                                  }
-                              }
-                          }
-                      })
-                  })
-              })
+            var provider_id = "{{ isset($servicedata->provider_id) ? $servicedata->provider_id : '' }}";
+            var provider_address_id = @json(isset($data) ? $data : []);
+    
+            var category_id = "{{ isset($servicedata->category_id) ? $servicedata->category_id : '' }}";
+            var subcategory_id = "{{ isset($servicedata->subcategory_id) ? $servicedata->subcategory_id : '' }}";
+    
+            var country_id = "{{ isset($servicedata->country_id) ? $servicedata->country_id : 0 }}";
+            var city_id = "{{ isset($servicedata->city_id) ? $servicedata->city_id : 0 }}";
+            var price_type = "{{ isset($servicedata->type) ? $servicedata->type : '' }}";
+    
+            providerAddress(provider_id, provider_address_id);
+            getSubCategory(category_id, subcategory_id);
+            priceformat(price_type);
+    
+            $(document).on('change', '#provider_id', function() {
+                var provider_id = $(this).val();
+                $('#provider_address_id').empty();
+                providerAddress(provider_id, provider_address_id);
+            });
+    
+            $(document).on('change', '#category_id', function() {
+                var category_id = $(this).val();
+                $('#subcategory_id').empty();
+                getSubCategory(category_id, subcategory_id);
+            });
+    
+            $(document).on('change', '#price_type', function() {
+                var price_type = $(this).val();
+                priceformat(price_type);
+            });
+    
+            $(document).on('change', '#country_id', function() {
+                var country = $(this).val();
+                $('#city_id').empty();
+                cityName(country);
+            });
+    
+            $(document).on('change', '#city_id', function() {
+                var city = $(this).val();
+                console.log('selected city', city);
+            });
+    
+            $('.galary').each(function(index, value) {
+                let galleryClass = $(value).attr('data-gallery');
+                $(galleryClass).magnificPopup({
+                    delegate: 'a#attachment_files',
+                    type: 'image',
+                    gallery: {
+                        enabled: true,
+                        navigateByImgClick: true,
+                        preload: [0, 1]
+                    },
+                    callbacks: {
+                        elementParse: function(item) {
+                            if (item.el[0].className.includes('video')) {
+                                item.type = 'iframe';
+                                item.iframe = {
+                                    markup: '<div class="mfp-iframe-scaler">' +
+                                        '<div class="mfp-close"></div>' +
+                                        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                                        '<div class="mfp-title">Some caption</div>' +
+                                        '</div>'
+                                };
+                            } else {
+                                item.type = 'image';
+                                item.tLoading = 'Loading image #%curr%...';
+                                item.mainClass = 'mfp-img-mobile';
+                                item.image = {
+                                    tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+                                };
+                            }
+                        }
+                    }
+                });
+            });
+        });
     </script>
+    
     @endsection
  </x-master-layout>
  
