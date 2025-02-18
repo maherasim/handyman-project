8fb01556a2a89307961b0744fb8daac195dedbc6
Asim Riaz <asimriazasim107@gmail.com>
2025-02-18 11:37:51 +0500

    job request field adjustment

--- resources/views/post-job-request/create.blade.php
+++ resources/views/post-job-request/create.blade.php
@@ -3,6 +3,7 @@
         referrerpolicy="origin"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12">
@@ -21,38 +22,44 @@
             <div class="col-lg-12">
                 <div class="card">
                     <div class="card-body">
-                        <form method="POST" action="{{ route('postJobRequest.save') }}" enctype="multipart/form-data" id="postJob">
+                        <form method="POST" action="{{ route('postJobRequest.save') }}" enctype="multipart/form-data"
+                            id="postJob">
                             @csrf
                             <input type="hidden" name="id" value="{{ old('id', $postJob->id ?? '') }}">
 
-                            <!-- First row with 4 fields -->
                             <div class="row">
                                 <div class="form-group col-md-2">
-                                    <label for="title">{{ __('messages.title') }} <span class="text-danger">*</span></label>
-                                    <input type="text" name="title" id="title" class="form-control" placeholder="{{ __('messages.title') }}" required>
+                                    <label for="title">{{ __('messages.title') }} <span
+                                            class="text-danger">*</span></label>
+                                    <input type="text" name="title" id="title" class="form-control"
+                                        placeholder="{{ __('messages.title') }}" required>
                                 </div>
 
                                 <div class="form-group col-md-2">
-                                    <label for="country_id">{{ __('messages.country') }} <span class="text-danger">*</span></label>
-                                    <select name="country_id" id="country_id" class="select2js form-group category" required></select>
+                                    <label for="country_id">{{ __('messages.country') }} <span
+                                            class="text-danger">*</span></label>
+                                    <select name="country_id" id="country_id" class="select2js form-group category"
+                                        required></select>
                                 </div>
 
                                 <div class="form-group col-md-2">
-                                    <label for="city_id">{{ __('messages.city') }} <span class="text-danger">*</span></label>
-                                    <select name="city_id" id="city_id" class="select2js form-group category" required></select>
+                                    <label for="city_id">{{ __('messages.city') }} <span
+                                            class="text-danger">*</span></label>
+                                    <select name="city_id" id="city_id" class="select2js form-group category"
+                                        required></select>
                                 </div>
 
-                                <div class="form-group col-md-3">
+                                <div class="form-group col-md-4">
                                     {{ html()->label(__('messages.select_name', ['select' => __('messages.category')]) . ' <span class="text-danger">*</span>', 'name')->class('form-control-label') }}
                                     <br />
-                                    {{ html()->select('category_id', [optional($servicedata->category)->id => optional($servicedata->category)->name], optional($servicedata->category)->id)
-                                        ->class('select2js form-group category')
-                                        ->required()
-                                        ->id('category_id')
-                                        ->attribute('data-placeholder', __('messages.select_name', ['select' => __('messages.category')]))
-                                        ->attribute('data-ajax--url', route('ajax-list', ['type' => 'category'])) }}
+                                    {{ html()->select(
+                                            'category_id',
+                                            [optional($servicedata->category)->id => optional($servicedata->category)->name],
+                                            optional($servicedata->category)->id,
+                                        )->class('select2js form-group category')->required()->id('category_id')->attribute('data-placeholder', __('messages.select_name', ['select' => __('messages.category')]))->attribute('data-ajax--url', route('ajax-list', ['type' => 'category'])) }}
+
                                 </div>
-                                <div class="form-group col-md-3">
+                                <div class="form-group col-md-4">
                                     {{ html()->label(__('messages.select_name', ['select' => __('messages.subcategory')]), 'subcategory_id')->class('form-control-label') }}
                                     <br />
                                     {{ html()->select('subcategory_id', $subcategories->pluck('name', 'id'), null)
@@ -60,16 +67,17 @@
                                         ->attribute('data-placeholder', __('messages.select_name', ['select' => __('messages.subcategory')])) }}
                                 </div>
                                 
+
                             </div>
 
-                            <!-- Second row with 4 fields -->
                             <div class="row">
-                                <div class="form-group col-md-3">
-                                    <label for="price">{{ __('messages.price') }} <span class="text-danger">*</span></label>
-                                    <input type="number" name="price" id="price" class="form-control" min="1" required>
+                                <div class="form-group col-md-2">
+                                    <label for="price">{{ __('messages.price') }} <span
+                                            class="text-danger">*</span></label>
+                                    <input type="number" name="price" id="price" class="form-control"
+                                        min="1" required>
                                 </div>
-
-                                <div class="form-group col-md-3">
+                                <div class="form-group col-md-2">
                                     <label for="price_type">{{ __('messages.price_type') }} <span class="text-danger">*</span></label>
                                     <select name="price_type" id="price_type" class="form-control" required>
                                         <option value="fixed">{{ __('Fixed') }}</option>
@@ -78,7 +86,7 @@
                                     </select>
                                 </div>
 
-                                <div class="form-group col-md-3">
+                                <div class="form-group col-md-2">
                                     <label for="job_type">{{ __('messages.job_type') }} <span class="text-danger">*</span></label>
                                     <select name="job_type" id="job_type" class="form-control" required>
                                         <option value="onsite">{{ __('Onsite') }}</option>
@@ -87,42 +95,35 @@
                                     </select>
                                 </div>
 
-                                <div class="form-group col-md-3">
-                                    <label for="start_date">{{ __('Start Date') }} <span class="text-danger">*</span></label>
-                                    <input type="date" name="start_date" id="start_date" class="form-control" required>
-                                </div>
-                            </div>
 
-                            <!-- Third row with 4 fields -->
-                            <div class="row">
-                                <div class="form-group col-md-3">
-                                    <label for="end_date">{{ __('End Date') }} <span class="text-danger">*</span></label>
+                                <div class="form-group col-md-2">
+                                    <label for="start_date">{{ __('Start Date') }} <span
+                                            class="text-danger">*</span></label>
+                                    <input type="date" name="start_date" id="start_date" class="form-control"
+                                        required>
+                                </div>
+
+                                <div class="form-group col-md-2">
+                                    <label for="end_date">{{ __('End Date') }} <span
+                                            class="text-danger">*</span></label>
                                     <input type="date" name="end_date" id="end_date" class="form-control" required>
                                 </div>
 
-                                <div class="form-group col-md-3">
+                                <!-- NEW TOTAL DAYS FIELD -->
+                                <div class="form-group col-md-2">
                                     <label for="total_days">{{ __('Total days') }}</label>
-                                    <input type="number" name="total_days" id="total_days" class="form-control" readonly>
+                                    <input type="number" name="total_days" id="total_days" class="form-control"
+                                        readonly>
                                 </div>
 
-                                <div class="form-group col-md-3">
-                                    <label for="total_hours">{{ __('Total Hours') }} <span class="text-danger">*</span></label>
-                                    <input type="number" name="total_hours" id="total_hours" class="form-control" readonly>
-                                </div>
-
-                                <!-- Multi-select Requirements Field -->
-                                <div class="form-group col-md-3">
-                                    <label for="requirements">{{ __('messages.requirements') }} <span class="text-danger">*</span></label>
-                                    <select name="requirements[]" id="requirements" class="form-control select2" multiple="multiple" required>
-                                        <option value="requirement_1">{{ __('Requirement 1') }}</option>
-                                        <option value="requirement_2">{{ __('Requirement 2') }}</option>
-                                        <option value="requirement_3">{{ __('Requirement 3') }}</option>
-                                        <option value="requirement_4">{{ __('Requirement 4') }}</option>
-                                    </select>
+                                <div class="form-group col-md-2">
+                                    <label for="total_hours">{{ __('Total Hours') }} <span
+                                            class="text-danger">*</span></label>
+                                    <input type="number" name="total_hours" id="total_hours" class="form-control"
+                                        readonly>
                                 </div>
                             </div>
 
-                            <!-- Fourth row with 2 fields -->
                             <div class="row">
                                 <div class="form-group col-md-6">
                                     <label for="description">{{ __('messages.description') }}</label>
@@ -130,20 +131,32 @@
                                 </div>
 
                                 <div class="form-group custom-file col-md-6 mt-30">
-                                    <input type="file" name="image[]" id="image" class="custom-file-input" accept="image/*" multiple>
+                                    <input type="file" name="image[]" id="image" class="custom-file-input"
+                                        accept="image/*" multiple>
                                     <label class="custom-file-label" for="image">{{ __('messages.image') }}</label>
                                     <div id="imageContainer"></div>
                                 </div>
+                                <div class="form-group col-md-4">
+                                    <label for="requirements">{{ __('messages.requirements') }} <span class="text-danger">*</span></label>
+                                    <select name="requirements[]" id="requirements" class="form-control select2" multiple="multiple" required>
+                                        <option value="requirement_1">{{ __('Requirement 1') }}</option>
+                                        <option value="requirement_2">{{ __('Requirement 2') }}</option>
+                                        <option value="requirement_3">{{ __('Requirement 3') }}</option>
+                                        <option value="requirement_4">{{ __('Requirement 4') }}</option>
+                                        <!-- Add more options as needed -->
+                                    </select>
+                                </div>
                             </div>
 
-                            <button type="submit" class="btn btn-md btn-primary float-right">{{ __('messages.save') }}</button>
+                            <button type="submit"
+                                class="btn btn-md btn-primary float-right">{{ __('messages.save') }}</button>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
- 
+
     @section('bottom_script')
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script>
@@ -263,14 +276,6 @@
                 toolbar: 'undo redo | bold italic | bullist numlist | link image preview',
                 menubar: false
             });
-            $(document).ready(function() {
-    // Initialize select2 for the requirements select
-    $('#requirements').select2({
-        placeholder: "{{ __('Select requirements') }}", // Optional placeholder
-        allowClear: true // Allows the user to clear selections
-    });
-});
-
         </script>
     @endsection
 @endsection
