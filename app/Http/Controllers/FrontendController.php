ccc0d4ad80717abf25ca34b0d24e20210aa044d4
Asim Riaz <asimriazasim107@gmail.com>
2025-02-18 16:00:14 +0500

    added country issue fixixng

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -498,8 +498,7 @@ class FrontendController extends Controller
         $apiRequest = new Request(['service_id' => $service_id, 'per_page' => 'all']);
         $service = $serviceController->getServiceDetail($apiRequest);
         $serviceData = json_decode($service->content(), true);
-        dd(json_encode($serviceData['service_detail']));
-
+ 
         $sitesetup = Setting::where('type','site-setup')->where('key', 'site-setup')->first();
         $date_time = json_decode($sitesetup->value);
         $favouriteServiceData = [];
