f9c09f064116beaab609d5f276fea62a6969da70
Asim Riaz <asimriazasim107@gmail.com>
2025-02-18 15:38:27 +0500

    added country issue fixixng

--- resources/views/service/create.blade.php
+++ resources/views/service/create.blade.php
@@ -194,77 +194,7 @@
                             </div>
                         </div>
 
-
-                        <div class="row service_attachment_div">
-                            <div class="col-md-12">
-
-
-                                @if (getMediaFileExit($servicedata, 'service_attachment'))
-                                    @php
-
-                                        $attchments = $servicedata->getMedia('service_attachment');
-
-                                        $file_extention = config('constant.IMAGE_EXTENTIONS');
-                                    @endphp
-                                    <div class="border-start">
-                                        <p class="ms-2"><b>{{ __('messages.attached_files') }}</b></p>
-                                        <div class="ms-2 my-3">
-                                            <div class="row">
-                                                @foreach ($attchments as $attchment)
-                                                    <?php
-                                                    $extention = in_array(strtolower(imageExtention($attchment->getFullUrl())), $file_extention);
-                                                    ?>
-
-                                                    <div class="col-md-2 pe-10 text-center galary file-gallary-{{ $servicedata->id }} position-relative"
-                                                        data-gallery=".file-gallary-{{ $servicedata->id }}"
-                                                        id="service_attachment_preview_{{ $attchment->id }}">
-                                                        @if ($extention)
-                                                            <a id="attachment_files"
-                                                                href="{{ $attchment->getFullUrl() }}"
-                                                                class="list-group-item-action attachment-list"
-                                                                target="_blank">
-                                                                <img src="{{ $attchment->getFullUrl() }}"
-                                                                    class="attachment-image" alt="">
-                                                            </a>
-                                                        @else
-                                                            <a id="attachment_files"
-                                                                class="video list-group-item-action attachment-list"
-                                                                href="{{ $attchment->getFullUrl() }}">
-                                                                <img src="{{ asset('images/file.png') }}"
-                                                                    class="attachment-file">
-                                                            </a>
-                                                        @endif
-                                                        <a class="text-danger remove-file"
-                                                            href="{{ route('remove.file', ['id' => $attchment->id, 'type' => 'service_attachment']) }}"
-                                                            data--submit="confirm_form" data--confirmation='true'
-                                                            data--ajax="true" data-toggle="tooltip"
-                                                            title='{{ __('messages.remove_file_title', ['name' => __('messages.attachments')]) }}'
-                                                            data-title='{{ __('messages.remove_file_title', ['name' => __('messages.attachments')]) }}'
-                                                            data-message='{{ __('messages.remove_file_msg') }}'>
-                                                            <i class="ri-close-circle-line"></i>
-                                                        </a>
-                                                    </div>
-                                                @endforeach
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group col-md-4">
-                                        <label class="form-control-label"
-                                            for="service_attachment">{{ __('messages.image') }}
-                                            <span class="text-danger">*</span>
-                                        </label>
-                                        <div class="custom-file">
-                                            <input type="file" onchange="preview()" name="service_attachment[]"
-                                                class="custom-file-input"
-                                                data-file-error="{{ __('messages.files_not_allowed') }}" multiple>
-                                            <label
-                                                class="custom-file-label upload-label">{{ __('messages.choose_file', ['file' => __('messages.attachments')]) }}</label>
-                                        </div>
-                                    </div>
-                                    <img id="service_attachment_preview" src="" width="150px" />
-                                @endif
-                            </div>
-                        </div>
+ 
 
                         <div class="row">
                             <div class="form-group col-md-12">
