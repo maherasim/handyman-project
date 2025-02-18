149efc786251ee0fc8740f8678871c83b254ac3c
Asim Riaz <asimriazasim107@gmail.com>
2025-02-18 19:14:12 +0500

    added handyman requirment job reuest

--- resources/views/landing-page/ServiceDetail.blade.php
+++ resources/views/landing-page/ServiceDetail.blade.php
@@ -71,10 +71,7 @@
                         <div class="mt-5 pt-lg-5 pt-3">
                             <h5 class="mb-3">{{ __('landingpage.about_service') }}</h5>
                             <p class="m-0">
-                                <div class="m-0">
-                                    {!! $serviceData['service_detail']['description'] !!}
-                                </div>
-                                
+                                {{ html_entity_decode(strip_tags($serviceData['service_detail']['description'])) }}
                             </p>
                         </div>
                     @endif
