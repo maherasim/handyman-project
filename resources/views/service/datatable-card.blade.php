193974c226dc68245a027033a4fb7275d496d5f3
Asim Riaz <asimriazasim107@gmail.com>
2025-02-19 23:32:37 +0500

    added git post job detail missing field

--- resources/views/service/datatable-card.blade.php
+++ resources/views/service/datatable-card.blade.php
@@ -104,8 +104,7 @@
    @endif 
    <a href="{{ route('service.detail', $data->id) }}"
       class="service-heading mt-2 d-block p-0">
-      <h5 class="service-heading service-title font-size-1 line-count-2"
-          style="font-size:15px">
+      <h5 class="service-heading text-capitalize"          style="font-size:15px">
           <h5>{{ Str::words($data->name, 3, '') }}</h5>
          </h5>
   </a>
