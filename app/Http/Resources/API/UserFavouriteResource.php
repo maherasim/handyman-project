e6a626e2ec4dd0bbaf7be98a00157284a43eaec5
Asim Riaz <asimriazasim107@gmail.com>
2025-04-29 15:52:56 +0500

    added

--- app/Http/Resources/API/UserFavouriteResource.php
+++ app/Http/Resources/API/UserFavouriteResource.php
@@ -36,12 +36,13 @@ class UserFavouriteResource extends JsonResource {
             ? ( float ) number_format( max( $this->service->serviceRating->avg( 'rating' ), 0 ), 2 )
             : 0,
 
-            'category_name' => optional(optional($this->service)->category)->name,
-
-            'category_id'   => $this->service->category_id,
-            'provider_image'=> optional( $this->service->providers )->login_type != null ? optional( $this->service->providers )->social_image : getSingleMedia( optional( $this->service->providers ), 'profile_image', null ),
-            'provider_name' => optional( $this->service->providers )->display_name,
-            'provider_id' => optional( $this->service->providers )->id
+            'category_name' => optional($service->category)->name,
+            'category_id'   => optional($service)->category_id,
+            'provider_image'=> $provider->login_type != null
+                                ? $provider->social_image
+                                : getSingleMedia($provider, 'profile_image', null),
+            'provider_name' => $provider->display_name,
+            'provider_id'   => $provider->id,
         ];
     }
 }
