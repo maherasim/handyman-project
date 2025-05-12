3ade49b352e5fb47ba5d069ea3c8adf953efce77
Asim Riaz <asimriazasim107@gmail.com>
2025-05-12 22:48:49 +0500

    added

--- resources/views/wallet/index.blade.php
+++ resources/views/wallet/index.blade.php
@@ -6,16 +6,21 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12">
-                <div class="card card-block card-stretch">
-                    <div class="card-body p-0">
-                        <div class="d-flex justify-content-between align-items-center p-3 flex-wrap gap-3">
-                            <h5 class="fw-bold">{{ $pageTitle ?? trans('messages.list') }}</h5>
-                            <a href="{{ route('wallet.create') }}" class=" float-end me-1 btn btn-sm btn-primary"><i class="fa fa-plus-circle"></i> {{ trans('messages.add_form_title',['form' => trans('messages.wallet')  ]) }}</a>
-                        </div>
-                       
-                    </div>
+    @if(auth()->user()->hasAnyRole(['admin']))
+        <div class="card card-block card-stretch">
+            <div class="card-body p-0">
+                <div class="d-flex justify-content-between align-items-center p-3 flex-wrap gap-3">
+                    <h5 class="fw-bold">{{ $pageTitle ?? trans('messages.list') }}</h5>
+                    <a href="{{ route('wallet.create') }}" class="float-end me-1 btn btn-sm btn-primary">
+                        <i class="fa fa-plus-circle"></i> 
+                        {{ trans('messages.add_form_title', ['form' => trans('messages.wallet')]) }}
+                    </a>
                 </div>
             </div>
+        </div>
+    @endif
+</div>
+
         </div>
     </div>
     <div class="card">
