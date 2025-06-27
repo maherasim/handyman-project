0a7a0bca62fe2949afd90d2930dde3943cdad396
Asim Riaz <asimriazasim107@gmail.com>
2025-06-27 10:32:14 +0500

    addedd

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -33,6 +33,13 @@ class WalletController extends Controller
         $assets = ['datatable'];
         return view('wallet.index', compact('pageTitle','auth_user','assets','filter'));
     }
+    public function cashIndex($id)
+    {
+        $pageTitle = __('messages.list_form_title',['form' => __('messages.cash_history')] );
+        $auth_user = authSession();
+        $assets = ['datatable'];
+        return view('paymenthistory.index', compact('pageTitle','assets','auth_user','id'));
+    }
 
 
     public function index_data(DataTables $datatable, Request $request)
@@ -151,7 +158,7 @@ class WalletController extends Controller
                 return getPriceFormat($payment->total_amount);
             })
             ->editColumn('history', function($payment) {
-                $action = '<a class="btn-link btn-link-hover" href="'.route('cash.index', $payment->id).'">'.__('messages.view').'</a>';
+                $action = '<a class="btn-link btn-link-hover" href="'.route('wallet.index', $payment->id).'">'.__('messages.view').'</a>';
                 return $action;
             })
             ->editColumn('status', function($query) {
--- routes/web.php
+++ routes/web.php
@@ -421,6 +421,8 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
 
     Route::resource('wallet', WalletController::class);
     Route::get('wallet-index-data', [WalletController::class, 'cash_index_data'])->name('wallet.index_data');
+    Route::get('wallet/history/{id?}', [WalletController::class, 'cashIndex'])->name('wallet.index');
+
     Route::post('wallet-bulk-action', [WalletController::class, 'bulk_action'])->name('wallet.bulk-action');
     Route::post('wallet/{id}', [WalletController::class, 'destroy'])->name('wallet.destroy');
     Route::get('wallet-history-index-data/{id}', [WalletController::class, 'wallethistory_index_data'])->name('wallethistory.index_data');
