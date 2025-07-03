7945e3084d0783dfc2097dd215909e4e117d9cbb
Asim Riaz <asimriazasim107@gmail.com>
2025-07-03 23:37:44 +0500

    added

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -1,9 +1,8 @@
 <x-master-layout>
+    @push('styles')
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+    @endpush
 
-    <head>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script type="text/javascript" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
-    </head>
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12">
@@ -16,15 +15,12 @@
                 </div>
             </div>
         </div>
-    </div>
-    <div class="card">
-        <div class="card-body">
-            <div class="row justify-content-between gy-3">
-              
-                <div class="col-md-6 col-lg-4 col-xl-3">
-                    <div class="d-flex align-items-center gap-3 justify-content-end">
-                        <div class="d-flex justify-content-end gap-3">
-                            
+
+        <div class="card mt-3">
+            <div class="card-body">
+                <div class="row justify-content-between gy-3">
+                    <div class="col-md-6 col-lg-4 col-xl-3">
+                        <div class="d-flex align-items-center gap-3 justify-content-end">
                             <div class="input-group input-group-search ms-2">
                                 <span class="input-group-text" id="addon-wrapping"><i class="fas fa-search"></i></span>
                                 <input type="text" class="form-control dt-search" placeholder="Search..."
@@ -34,36 +30,40 @@
                         </div>
                     </div>
                 </div>
-                <div class="table-responsive">
-                    <table id="datatable" class="table table-striped border">
-                    </table>
+
+                <div class="table-responsive mt-4">
+                    <table id="datatable" class="table table-striped border w-100"></table>
                 </div>
             </div>
         </div>
     </div>
-    <script>
-        document.addEventListener('DOMContentLoaded', (event) => {
 
-            window.renderedDataTable = $('#datatable').DataTable({
-                processing: true,
-                serverSide: true,
-                autoWidth: false,
-                responsive: true,
-                dom: '<"row align-items-center"><"table-responsive my-3 mt-3 mb-2 pb-1" rt><"row align-items-center data_table_widgets" <"col-md-6" <"d-flex align-items-center flex-wrap gap-3" l i>><"col-md-6" p>><"clear">',
-                ajax: {
-                    "type": "GET",
-                    "url": '{{ route('transaction-request.index_data') }}',
-                    "data": function(d) {
-                        d.search = {
-                            value: $('.dt-search').val()
-                        };
-                        d.filter = {
-                            column_status: $('#column_status').val()
-                        }
+    @push('scripts')
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+        <script>
+            document.addEventListener('DOMContentLoaded', () => {
+                window.renderedDataTable = $('#datatable').DataTable({
+                    processing: true,
+                    serverSide: true,
+                    autoWidth: false,
+                    responsive: true,
+                    ajax: {
+                        type: 'GET',
+                        url: '{{ route('transaction-request.index_data') }}',
+                        data: function(d) {
+                            d.search = {
+                                value: $('.dt-search').val()
+                            };
+                            d.filter = {
+                                column_status: $('#column_status').val()
+                            }
+                        },
                     },
-                },
-                columns: [
-                    @if (auth()->user()->hasAnyRole(['admin']))
+                    columns: [
+                        @if (auth()->user()->hasAnyRole(['admin']))
                         {
                             name: 'check',
                             data: 'check',
@@ -72,53 +72,51 @@
                             orderable: false,
                             searchable: false,
                         },
-                    @endif () {
-                        data: 'updated_at',
-                        name: 'updated_at',
-                        title: "{{ __('product.lbl_update_at') }}",
-                        orderable: true,
-                        visible: false,
-                    },
-                    {
-                        data: 'id',
-                        name: 'id',
-                        title: "{{ __('messages.id') }}"
-                    },
-                    {
-                        data: 'user_id',
-                        name: 'user_id',
-                        title: "Name"
-                    },
-                    {
-                        data: 'amount',
-                        name: 'amount',
-                        title: "Amount",
-                        orderable: false,
-                    },
-                    {
-                        data: 'transaction_type',
-                        name: 'transaction_type',
-                        title: "{{ __('messages.transaction_type') }}",
-                        orderable: false,
-                        searchable: false
-                    },
-                    {
-                        data: 'created_at',
-                        name: 'created_at',
-                        title: "{{ __('messages.created_at') }}",
-                        orderable: true,
-                    },
-                   
-                    {
-                        data: 'status',
-                        name: 'status',
-                        title: "{{ __('messages.status') }}",
-                        orderable: false,
-                        searchable: false,
-                    },
-
-                   
-                    @if (auth()->user()->hasAnyRole(['admin']))
+                        @endif
+                        {
+                            data: 'updated_at',
+                            name: 'updated_at',
+                            title: "{{ __('product.lbl_update_at') }}",
+                            orderable: true,
+                            visible: false,
+                        },
+                        {
+                            data: 'id',
+                            name: 'id',
+                            title: "{{ __('messages.id') }}"
+                        },
+                        {
+                            data: 'user_id',
+                            name: 'user_id',
+                            title: "Name"
+                        },
+                        {
+                            data: 'amount',
+                            name: 'amount',
+                            title: "Amount",
+                            orderable: false,
+                        },
+                        {
+                            data: 'transaction_type',
+                            name: 'transaction_type',
+                            title: "{{ __('messages.transaction_type') }}",
+                            orderable: false,
+                            searchable: false
+                        },
+                        {
+                            data: 'created_at',
+                            name: 'created_at',
+                            title: "{{ __('messages.created_at') }}",
+                            orderable: true,
+                        },
+                        {
+                            data: 'status',
+                            name: 'status',
+                            title: "{{ __('messages.status') }}",
+                            orderable: false,
+                            searchable: false,
+                        },
+                        @if (auth()->user()->hasAnyRole(['admin']))
                         {
                             data: 'action',
                             name: 'action',
@@ -126,82 +124,20 @@
                             searchable: false,
                             title: "{{ __('messages.action') }}"
                         }
-                    @endif ()
-
-                ],
-                order: [
-                    @if (auth()->user()->hasAnyRole(['admin']))
-                        [5, 'desc']
-                    @else
-                        [4, 'desc']
-                    @endif
-                ],
-                language: {
-                    processing: "{{ __('messages.processing') }}" // Set your custom processing text
-                }
-
+                        @endif
+                    ],
+                    order: [
+                        @if (auth()->user()->hasAnyRole(['admin']))
+                            [5, 'desc']
+                        @else
+                            [4, 'desc']
+                        @endif
+                    ],
+                    language: {
+                        processing: "{{ __('messages.processing') }}"
+                    }
+                });
             });
-        });
-
-        $(document).ready(function() {
-            $('#statusSelect').change(function() {
-                var selectedValue = $(this).val();
-                var selectedOption = $('#statusSelect option:selected');
-                var route = selectedOption.data('route');
-
-                if (selectedValue === 'cash' && route) {
-                    window.location.href = route;
-                }
-                window.location.href = route;
-            });
-        });
-
-        function resetQuickAction() {
-            const actionValue = $('#quick-action-type').val();
-            console.log(actionValue)
-            if (actionValue != '') {
-                $('#quick-action-apply').removeAttr('disabled');
-
-                if (actionValue == 'change-status') {
-                    $('.quick-action-field').addClass('d-none');
-                    $('#change-status-action').removeClass('d-none');
-                } else {
-                    $('.quick-action-field').addClass('d-none');
-                }
-            } else {
-                $('#quick-action-apply').attr('disabled', true);
-                $('.quick-action-field').addClass('d-none');
-            }
-        }
-
-        $('#quick-action-type').change(function() {
-            resetQuickAction()
-        });
-
-        $(document).on('update_quick_action', function() {
-
-        })
-
-        $(document).on('click', '[data-ajax="true"]', function(e) {
-            e.preventDefault();
-            const button = $(this);
-            const confirmation = button.data('confirmation');
-
-            if (confirmation === 'true') {
-                const message = button.data('message');
-                if (confirm(message)) {
-                    const submitUrl = button.data('submit');
-                    const form = button.closest('form');
-                    form.attr('action', submitUrl);
-                    form.submit();
-                }
-            } else {
-                const submitUrl = button.data('submit');
-                const form = button.closest('form');
-                form.attr('action', submitUrl);
-                form.submit();
-            }
-        });
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+        </script>
+    @endpush
 </x-master-layout>
