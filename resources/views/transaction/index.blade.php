77e05cc07178d27d8116c2e7b97515fa6b066e94
Asim Riaz <asimriazasim107@gmail.com>
2025-07-03 23:41:01 +0500

    added

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -19,7 +19,26 @@
     </div>
     <div class="card">
         <div class="card-body">
-            
+            <div class="row justify-content-between ">
+              
+                <div class="col-md-6 col-lg-4 col-xl-3">
+                    <div class="d-flex align-items-center gap-3 justify-content-end">
+                        <div class="d-flex justify-content-end gap-3">
+                            
+                            <div class="input-group input-group-search ms-2">
+                                <span class="input-group-text" id="addon-wrapping"><i class="fas fa-search"></i></span>
+                                <input type="text" class="form-control dt-search" placeholder="Search..."
+                                    aria-label="Search" aria-describedby="addon-wrapping"
+                                    aria-controls="dataTableBuilder">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="table-responsive">
+                    <table id="datatable" class="table table-striped border">
+                    </table>
+                </div>
+            </div>
             <div class="row">
     <div class="col-12">
         <div class="d-flex justify-content-end">
