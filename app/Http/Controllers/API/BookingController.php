6d14ae8e70a041faffb66bef8208da86633b3c15
Asim Riaz <asimriazasim107@gmail.com>
2025-07-13 20:44:12 +0500

    added

--- app/Http/Controllers/API/BookingController.php
+++ app/Http/Controllers/API/BookingController.php
@@ -470,7 +470,14 @@ class BookingController extends Controller
                     'qty'   => $extra['qty'],
                     'booking_id'   =>$bookingdata->id,
                 ];
-                $bookingdata->bookingExtraCharge()->insert($extra_charge);
+                foreach ($request->extra_charges as $extra) {
+                    $bookingdata->bookingExtraCharge()->create([
+                        'title'      => $extra['title'],
+                        'price'      => $extra['price'],
+                        'qty'        => $extra['qty'],
+                    ]);
+                }
+
             }
             $subtotal = $bookingdata->getSubTotalValue() + $bookingdata->getServiceAddonValue() + $bookingdata->getExtraChargeValue();
 
