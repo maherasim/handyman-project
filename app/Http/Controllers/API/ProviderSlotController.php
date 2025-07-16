5d990b10900bcce89a643dfee0e3cb6ba98c39d5
Asim Riaz <asimriazasim107@gmail.com>
2025-07-16 12:41:49 +0500

    added full calenar api

--- app/Http/Controllers/API/ProviderSlotController.php
+++ app/Http/Controllers/API/ProviderSlotController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
 use App\Models\ProviderSlotMapping;
 
 class ProviderSlotController extends Controller
@@ -43,6 +44,47 @@ class ProviderSlotController extends Controller
     }
 
     return comman_custom_response($calendarSlots);
+}
+public function store(Request $request)
+{
+    $request->validate([
+        'slots' => 'required|array',
+        'slots.*.day' => 'required|string|in:sun,mon,tue,wed,thu,fri,sat',
+        'slots.*.time' => 'required|array',
+        'slots.*.time.*' => 'required|date_format:H:i',
+    ]);
+
+    $slotdata = $request->all();
+    $provider_id = $request->provider_id ?? auth()->user()->id;
+
+    // Delete old slots for this provider
+    ProviderSlotMapping::where('provider_id', $provider_id)->delete();
+
+    $isCreated = false;
+
+    foreach ($slotdata['slots'] as $value) {
+        if (!empty($value['time'])) {
+            foreach ($value['time'] as $time) {
+                $start = \Carbon\Carbon::createFromFormat('H:i', $time);
+                $end = $start->copy()->addMinutes(60); // or 30, your choice
+
+                ProviderSlotMapping::create([
+                    'provider_id' => $provider_id,
+                    'days' => $value['day'],
+                    'start_at' => $start->format('H:i'),
+                    'end_at' => $end->format('H:i')
+                ]);
+
+                $isCreated = true;
+            }
+        }
+    }
+
+    $message = $isCreated
+        ? __('messages.save_form', ['form' => __('messages.providerslot')])
+        : __('messages.update_form', ['form' => __('messages.providerslot')]);
+
+    return comman_message_response($message);
 }
 
 }
