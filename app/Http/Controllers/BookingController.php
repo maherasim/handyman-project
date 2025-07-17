7a89e48dc9902bb497c19121c5e1df4bad7d7aeb
Asim Riaz <asimriazasim107@gmail.com>
2025-07-17 23:19:35 +0500

    undo code

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -1023,7 +1023,8 @@ public function bookingAssigned(Request $request)
         $global_advance_payment = isset($serviceconfig->global_advance_payment) ? $serviceconfig->global_advance_payment : 0;
         $bookingdata->service->is_enable_advance_payment = $bookingdata->service->type == 'fixed' ? ($bookingdata->service->is_enable_advance_payment == 1 ? $bookingdata->service->is_enable_advance_payment : $global_advance_payment) : 0;
         $bookingdata->service->advance_payment_amount = $bookingdata->service->type == 'fixed' ? ($bookingdata->service->advance_payment_amount > 0 ? $bookingdata->service->advance_payment_amount : $advancePaymentPercentage) : 0;
-dd( $bookingdata->status);
+dd(auth()->user()->user_type);
+
         switch ($tabpage) {
             case 'info':
                 $data = view('booking.' . $tabpage, compact('user_data', 'tabpage', 'auth_user', 'bookingdata', 'payment', 'customer_review', 'serviceProof', 'is_enable_advance_payment'))->render();
