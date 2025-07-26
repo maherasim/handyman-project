ac093f96ac70bb08bbcbc96505df6c08460b170c
Asim Riaz <asimriazasim107@gmail.com>
2025-07-26 19:23:59 +0500

    addded something

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -379,6 +379,8 @@ public function getpaymentall(Request $request)
             $advance_paid = $booking->advance_paid_amount ?? 0;
             $total_amount = $booking->total_amount;
             $remaining_amount = $total_amount - $advance_paid;
+            $result->total_amount = $remaining_amount;
+            $result->save();
 
             $admin_commission_percentage = Setting::getValueByKey('admin_commission_percentage', 'site-setup')->value ?? 10;
             $admin_user_id = User::where('user_type', 'admin')->value('id');
