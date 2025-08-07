de790f29145ceb2a1771699948c85d703e59572b
Asim Riaz <asimriazasim107@gmail.com>
2025-08-07 14:09:42 +0500

    added something

--- resources/views/service/create.blade.php
+++ resources/views/service/create.blade.php
@@ -803,5 +803,251 @@
                 menubar: false
             })
         </script>
+         <script type="text/javascript">
+            function preview() {
+                service_attachment_preview.src = URL.createObjectURL(event.target.files[0]);
+            }
+            var discountInput = document.getElementById('discount');
+            var discountError = document.getElementById('discount-error');
+
+
+            document.addEventListener('DOMContentLoaded', function() {
+                var initialProviderId = document.getElementById('provider_id').value;
+                selectprovider({
+                    value: initialProviderId
+                });
+                document.getElementById('add_provider_address_link').addEventListener('click', function(event) {
+                    event.preventDefault();
+                    var providerId = document.getElementById('provider_id').value;
+                    var providerAddressCreateUrl =
+                        "{{ route('provideraddress.create', ['provideraddress' => '']) }}";
+                    providerAddressCreateUrl = providerAddressCreateUrl.replace('provideraddress=',
+                        'provideraddress=' + providerId);
+                    window.location.href = providerAddressCreateUrl;
+                });
+
+
+
+
+
+            });
+
+            function selectprovider(selectElement) {
+
+                var providerId = selectElement.value;
+                var addProviderAddressLink = document.getElementById('add_provider_address_link');
+
+                if (providerId) {
+                    addProviderAddressLink.classList.remove('d-none');
+                } else {
+                    addProviderAddressLink.classList.add('d-none');
+                }
+            }
+
+
+            discountInput.addEventListener('input', function() {
+                var discountValue = parseFloat(discountInput.value);
+                if (isNaN(discountValue) || discountValue < 0 || discountValue > 99) {
+                    discountError.textContent = "{{ __('Discount value should be between 0 to 99') }}";
+                } else {
+                    discountError.textContent = "";
+                }
+            });
+
+            var isEnableAdvancePayment = $("input[name='is_enable_advance_payment']").prop('checked');
+
+            var priceType = $("#price_type").val();
+
+            enableAdvancePayment(priceType);
+            checkEnablePayment(isEnableAdvancePayment);
+
+            $("#is_enable_advance_payment").change(function() {
+                isEnableAdvancePayment = $(this).prop('checked');
+                checkEnablePayment(isEnableAdvancePayment);
+                updateAmountVisibility(priceType, isEnableAdvancePayment);
+            });
+
+            $("#price_type").change(function() {
+                priceType = $(this).val();
+                enableAdvancePayment(priceType);
+                updateAmountVisibility(priceType, isEnableAdvancePayment);
+            });
+
+            function checkEnablePayment(value) {
+                $("#amount").toggleClass('d-none', !value);
+                $('#advance_payment_amount').prop('required', value);
+            }
+
+            function enableAdvancePayment(type) {
+                $("#is_enable_advance").toggleClass('d-none', type !== 'fixed');
+            }
+
+            function updateAmountVisibility(type, isEnableAdvancePayment) {
+                if (type === 'fixed' && !$("#is_enable_advance").hasClass('d-none') && isEnableAdvancePayment) {
+                    $("#amount").removeClass('d-none');
+                } else {
+                    $("#amount").addClass('d-none');
+                }
+            }
+
+            (function($) {
+                "use strict";
+                $(document).ready(function() {
+                    var provider_id = "{{ isset($servicedata->provider_id) ? $servicedata->provider_id : '' }}";
+                    var provider_address_id = "{{ isset($data) ? $data : [] }}";
+
+                    var category_id = "{{ isset($servicedata->category_id) ? $servicedata->category_id : '' }}";
+                    var subcategory_id =
+                        "{{ isset($servicedata->subcategory_id) ? $servicedata->subcategory_id : '' }}";
+
+                    var price_type = "{{ isset($servicedata->type) ? $servicedata->type : '' }}";
+
+                    providerAddress(provider_id, provider_address_id)
+                    getSubCategory(category_id, subcategory_id)
+                    priceformat(price_type)
+
+                    $(document).on('change', '#provider_id', function() {
+                        var provider_id = $(this).val();
+                        $('#provider_address_id').empty();
+                        providerAddress(provider_id, provider_address_id);
+                    })
+                    $(document).on('change', '#category_id', function() {
+                        var category_id = $(this).val();
+                        $('#subcategory_id').empty();
+                        getSubCategory(category_id, subcategory_id);
+                    })
+                    $(document).on('change', '#price_type', function() {
+                        var price_type = $(this).val();
+                        priceformat(price_type);
+                    })
+
+
+                    $('.galary').each(function(index, value) {
+                        let galleryClass = $(value).attr('data-gallery');
+                        $(galleryClass).magnificPopup({
+                            delegate: 'a#attachment_files',
+                            type: 'image',
+                            gallery: {
+                                enabled: true,
+                                navigateByImgClick: true,
+                                preload: [0,
+                                    1
+                                ] // Will preload 0 - before current, and 1 after the current image
+                            },
+                            callbacks: {
+                                elementParse: function(item) {
+                                    if (item.el[0].className.includes('video')) {
+                                        item.type = 'iframe',
+                                            item.iframe = {
+                                                markup: '<div class="mfp-iframe-scaler">' +
+                                                    '<div class="mfp-close"></div>' +
+                                                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                                                    '<div class="mfp-title">Some caption</div>' +
+                                                    '</div>'
+                                            }
+                                    } else {
+                                        item.type = 'image',
+                                            item.tLoading = 'Loading image #%curr%...',
+                                            item.mainClass = 'mfp-img-mobile',
+                                            item.image = {
+                                                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+                                            }
+                                    }
+                                }
+                            }
+                        })
+                    })
+                })
+
+                function providerAddress(provider_id, provider_address_id = "") {
+                    var provider_address_route =
+                        "{{ route('ajax-list', ['type' => 'provider_address', 'provider_id' => '']) }}" + provider_id;
+                    provider_address_route = provider_address_route.replace('amp;', '');
+
+                    $.ajax({
+                        url: provider_address_route,
+                        success: function(result) {
+                            $('#provider_address_id').select2({
+                                width: '100%',
+                                placeholder: "{{ trans('messages.select_name', ['select' => trans('messages.provider_address')]) }}",
+                                data: result.results
+                            });
+                            if (provider_address_id != "") {
+                                $('#provider_address_id').val(provider_address_id.split(',')).trigger('change');
+                            }
+                        }
+                    });
+                }
+
+                function getSubCategory(category_id, subcategory_id = "") {
+                    var get_subcategory_list =
+                        "{{ route('ajax-list', ['type' => 'subcategory_list', 'category_id' => '']) }}" + category_id;
+                    get_subcategory_list = get_subcategory_list.replace('amp;', '');
+
+                    $.ajax({
+                        url: get_subcategory_list,
+                        success: function(result) {
+                            $('#subcategory_id').select2({
+                                width: '100%',
+                                placeholder: "{{ trans('messages.select_name', ['select' => trans('messages.subcategory')]) }}",
+                                data: result.results
+                            });
+                            if (subcategory_id != "") {
+                                $('#subcategory_id').val(subcategory_id).trigger('change');
+                            }
+                        }
+                    });
+                }
+                var price = "{{ isset($servicedata->price) ? $servicedata->price : '' }}";
+                var discount = "{{ isset($servicedata->discount) ? $servicedata->discount : '' }}";
+
+                function priceformat(value) {
+                    if (value == 'free') {
+                        $('#price').val(0);
+                        $('#price').attr("readonly", true)
+
+                        $('#discount').val(0);
+                        $('#discount').attr("readonly", true)
+
+                    } else {
+                        $('#price').val(price);
+                        $('#price').attr("readonly", false)
+                        $('#discount').val(discount);
+                        $('#discount').attr("readonly", false)
+                    }
+                }
+            })(jQuery);
+
+            document.addEventListener('DOMContentLoaded', function() {
+                checkImage();
+            });
+
+            function checkImage() {
+                var id = @json($servicedata->id);
+                var route = "{{ route('check-image', ':id') }}";
+                route = route.replace(':id', id);
+                var type = 'service';
+
+                $.ajax({
+                    url: route,
+                    type: 'GET',
+                    data: {
+                        type: type,
+                    },
+                    success: function(result) {
+                        var attachments = result.results;
+
+                        if (attachments.length === 0) {
+                            $('input[name="service_attachment[]"]').attr('required', 'required');
+                        } else {
+                            $('input[name="service_attachment[]"]').removeAttr('required');
+                        }
+                    },
+                    error: function(xhr, status, error) {
+                        console.error('Error:', error);
+                    }
+                });
+            }
+        </script>
     @endsection
 </x-master-layout>
