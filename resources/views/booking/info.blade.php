8b991f964e15f21e25b59e507463856a6ce0f38a
Asim Riaz <asimriazasim107@gmail.com>
2025-08-10 14:05:30 +0500

    added

--- resources/views/booking/info.blade.php
+++ resources/views/booking/info.blade.php
@@ -572,7 +572,7 @@
                                     <div class="card-body">
                                         <p class="opacity-75 fz-12">{{ __('Working Address') }}</p>
                                         <p class="mb-0 text-primary">
-                                            {{ ($bookingdata->address) : '-' }}</p>
+                                             {{ isset($payment) ? ucfirst($bookingdata->address) : '-' }}</p>
                                     </div>
                                 </div>
                             </div>
