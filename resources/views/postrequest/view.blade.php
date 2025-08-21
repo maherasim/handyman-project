c91a53f71ead3942f3e7dfb1c1811588ba9a446b
Asim Riaz <asimriazasim107@gmail.com>
2025-08-21 12:49:49 +0500

    added confirm button

--- resources/views/postrequest/view.blade.php
+++ resources/views/postrequest/view.blade.php
@@ -2,9 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12">
-
-                <!-- Page Header -->
-             <div class="card card-block card-stretch mb-3"> 
+ <div class="card card-block card-stretch mb-3"> 
     <div class="card-body d-flex justify-content-between align-items-center">
         <h5 class="fw-bold mb-0">{{ $pageTitle }}</h5>
 
@@ -33,6 +31,7 @@
     </div>
 </div>
 
+               
         </div>
     </div>
 
