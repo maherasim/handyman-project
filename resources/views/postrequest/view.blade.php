41e157646cc58b398f106331191daa39d0813e22
Asim Riaz <asimriazasim107@gmail.com>
2025-08-21 12:12:24 +0500

    added confirm button

--- resources/views/postrequest/view.blade.php
+++ resources/views/postrequest/view.blade.php
@@ -14,7 +14,7 @@
                                 <i class="fas fa-play"></i> Set Payment
                             </button>
                         @endif  
-                         @if (isset($advance_payment) && auth()->id() === $advance_payment->customer_id)
+                         @if (isset($advance_payment))
                             <button class="btn btn-primary startWorkBtn" data-post-id="{{ $advance_payment->id }}">
                                 <i class="fas fa-play"></i> Pay Advance 
                             </button>
