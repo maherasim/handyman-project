8e6aac546917d3290dfc187b25c941ab47351ec9
Asim Riaz <asimriazasim107@gmail.com>
2025-09-03 22:00:20 +0500

    adas

--- resources/views/postrequest/index.blade.php
+++ resources/views/postrequest/index.blade.php
@@ -3,8 +3,7 @@
     <head>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script type="text/javascript" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    </head>
+     </head>
     <div class="container-fluid">
         <div class="row">
             @if (session('success'))
@@ -454,7 +453,6 @@
             window.initializeWhyChooseMeEditor = initializeWhyChooseMeEditor;
         });
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 </x-master-layout>
