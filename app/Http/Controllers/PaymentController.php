5face1226ba9c1c97ca85b686fb813daa439c6a6
Asim Riaz <asimriazasim107@gmail.com>
2025-09-10 11:45:33 +0500

    added

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -150,7 +150,7 @@ class PaymentController extends Controller
 
     public function paymentjobrequest_history_data(DataTables $datatable, $id)
     {
-        $query = PaymentPostJob::where('id', $id);
+        $query = PaymentPostJobHistory::where('id', $id);
 
         if (auth()->user()->hasAnyRole(['admin'])) {
             $query->newQuery();
