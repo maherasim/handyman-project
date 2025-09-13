85a284be0b8a321d054e864d22c0157a6ddb419a
Asim Riaz <asimriazasim107@gmail.com>
2025-09-13 22:17:51 +0500

    aded

--- app/Http/Controllers/PostJobRequestController.php
+++ app/Http/Controllers/PostJobRequestController.php
@@ -148,7 +148,7 @@ dd($bid->postrequest);
         $bid = PostJobBid::with([
             'provider:id,display_name',
             'customer:id,display_name',
-            'postrequest',
+            'postrequest:id,title,customer_id,status,provider_id,remaining_percent,type,start_date,end_date,total_budget,city_id,country_id,job_price,working_address,total_hours',
             'postrequest.city:id,name',
             'postrequest.country:id,name',
             'postrequest.postBidList:id,post_request_id',
