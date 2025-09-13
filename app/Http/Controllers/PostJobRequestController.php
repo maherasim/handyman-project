1e539bd9660ad642b3950d1cbe549905e36c051d
Asim Riaz <asimriazasim107@gmail.com>
2025-09-13 22:10:21 +0500

    aded

--- app/Http/Controllers/PostJobRequestController.php
+++ app/Http/Controllers/PostJobRequestController.php
@@ -119,11 +119,12 @@ class PostJobRequestController extends Controller
         $bidQuery = PostJobBid::with([
             'provider:id,display_name',
             'customer:id,display_name',
-            'postrequest:id,title,customer_id,status,provider_id,remaining_percent,type,start_date,end_date,total_budget,city_id,country_id,job_price,working_address','total_hours','total_days',
+            'postrequest:id,title,customer_id,status,provider_id,remaining_percent,type,start_date,end_date,total_budget,city_id,country_id,job_price,working_address,total_hours,total_days',
             'postrequest.city:id,name',
             'postrequest.country:id,name',
             'postrequest.postBidList:id,post_request_id',
         ])->where('post_request_id', $postRequestId);
+        
 
         // Try to get bid matching the post's current status
         $bid = null;
