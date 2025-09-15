15445d3c1a75945544142058a2637d4b0830ba83
Asim Riaz <asimriazasim107@gmail.com>
2025-09-15 20:30:06 +0500

    aded

--- resources/views/postrequest/show.blade.php
+++ resources/views/postrequest/show.blade.php
@@ -291,7 +291,7 @@
                             } else {
                                 $quantity = (float) ($bid->quantity ?? 1);
                             }
-@dd($bid->postrequest->price_type);
+@dd($bid->postrequest);
                             // Calculations
                             $totalAmount = $unitPrice * $quantity;
                             $extraChargesTotal = $extraChargeUnit * $extraChargeQty;
