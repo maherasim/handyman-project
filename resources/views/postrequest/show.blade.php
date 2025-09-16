58c2123eb754a93b618acd9b574a70cecc303ef9
Asim Riaz <asimriazasim107@gmail.com>
2025-09-16 11:14:02 +0500

    testing

--- resources/views/postrequest/show.blade.php
+++ resources/views/postrequest/show.blade.php
@@ -217,15 +217,21 @@
                     
                     @endif
 
-                    <div class="col-md-4">
+                    <div class="col-md-4"> 
                         <div class="card border-secondary shadow-sm h-100 hover-shadow">
                             <div class="card-body text-center">
                                 <i class="fas fa-wallet fa-2x text-secondary mb-2"></i>
                                 <h6 class="fw-bold mb-1">Total Budget</h6>
-                                <p class="mb-0">{{ $bid->postrequest->total_budget ?? '-' }}</p>
+                                <p class="mb-0">
+                                    {{ isset($bid->postrequest->total_budget) 
+                                        ? number_format($bid->postrequest->total_budget, 2, '.', '') . ' €' 
+                                        : '-' 
+                                    }}
+                                </p>
                             </div>
                         </div>
                     </div>
+                    
 
                     <div class="col-md-4">
                         <div class="card border-dark shadow-sm h-100 hover-shadow">
