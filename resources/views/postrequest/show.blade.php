09dda29ea211e348ba60b9d96baba93505497f7a
Asim Riaz <asimriazasim107@gmail.com>
2025-09-18 14:05:08 +0500

    added

--- resources/views/postrequest/show.blade.php
+++ resources/views/postrequest/show.blade.php
@@ -315,10 +315,10 @@
                                     <td>Rate (Unit Price)</td>
                                     <td class="text-end">€{{ number_format($unitPrice, 2) }}</td>
                                 </tr>
-                              
+                                @dd($bid->postrequest->total_days, $quantity );
                                 <tr>
                                     <td>Quantity (Packages / Hours / Days)</td>
-                                    <td class="text-end">{{ $quantity }} </td>
+                                    <td class="text-end">{{ $quantity }}</td>
                                 </tr>
                                 <tr>
                                     <td>Total Amount</td>
