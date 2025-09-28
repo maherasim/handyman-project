fc16260ebf0fa374485b15089ccae4cf03e75ea2
Asim Riaz <asimriazasim107@gmail.com>
2025-09-28 08:54:52 +0500

    aded

--- resources/views/partials/_body_header.blade.php
+++ resources/views/partials/_body_header.blade.php
@@ -52,7 +52,7 @@
                                 <span id="chatPulse" class="position-absolute chat-pulse-dot" style="top:-2px; right:-2px; display:none;"></span>
                             </a>
                         </li>
-                        <li class="nav-item nav-icon dropdown">
+                        {{-- <li class="nav-item nav-icon dropdown">
                             <a href="#" class="search-toggle dropdown-toggle notification_list" id="notification-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" class="h-6 w-6 text-secondary" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
@@ -69,7 +69,7 @@
                             <div class="iq-sub-dropdown dropdown-menu " aria-labelledby="notification-dropdown">
                                 <div class="card shadow-none m-0 border-0 notification_data"></div>
                             </div>
-                        </li>
+                        </li> --}}
                         <li class="nav-item nav-icon dropdown">
                             <a href="#" class="search-toggle dropdown-toggle language-toggle" id="languageDropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <?php
