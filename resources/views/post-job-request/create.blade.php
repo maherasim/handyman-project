216fb5fdeb4a576ce44cbaa4e5d05fe9c9320bd3
Asim Riaz <asimriazasim107@gmail.com>
2025-09-29 16:30:13 +0500

    aded

--- resources/views/post-job-request/create.blade.php
+++ resources/views/post-job-request/create.blade.php
@@ -192,19 +192,22 @@
                                         <option value="doctorate" {{ $oldEdu == 'doctorate' ? 'selected' : '' }}>{{ __('Doctorate') }}</option>
                                     </select>
                                 </div>
-                                <div class="col-md-12">
-                                    <h6 class="font-weight-bold mb-2">{{ __('Working Address') }}</h6>
-                                    <p class="text-muted small mb-3">{{ __('Specify where the work will be performed.') }}</p>
-                                </div>
+                            
                                 <div class="form-group col-md-4">
                                     <label for="street_address">{{ __('Street & House Nr.') }}</label>
-                                    <input type="text" name="street_address" id="street_address" class="form-control" placeholder="{{ __('Street name') }}" value="{{ old('street_address', $postJob->street_address) }}">
+                                    <input type="text" name="street_address" id="street_address" class="form-control"
+                                           placeholder="{{ __('Street name') }}"
+                                           value="{{ old('street_address', $postJob->street_address) }}">
                                 </div>
+                            
                                 <div class="form-group col-md-4">
                                     <label for="house_number">{{ __('Pobox & City-Country.') }}</label>
-                                    <input type="text" name="house_number" id="house_number" class="form-control" placeholder="{{ __('House/Unit No.') }}" value="{{ old('house_number', $postJob->house_number) }}">
+                                    <input type="text" name="house_number" id="house_number" class="form-control"
+                                           placeholder="{{ __('House/Unit No.') }}"
+                                           value="{{ old('house_number', $postJob->house_number) }}">
                                 </div>
                             </div>
+                            
 
                             <div class="row">
                                 <div class="form-group col-md-3">
