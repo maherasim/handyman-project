fedb5e9a4ad6feaea9da53693b279142f7663486
Asim Riaz <asimriazasim107@gmail.com>
2025-09-29 16:23:20 +0500

    aded

--- app/Http/Controllers/PostJobRequestController.php
+++ app/Http/Controllers/PostJobRequestController.php
@@ -1633,6 +1633,7 @@ class PostJobRequestController extends Controller
         ]);
 
         $data = $request->all();
+        dd(  $data);
         // store new address parts if present
         $data['street_address'] = $request->input('street_address');
         $data['house_number'] = $request->input('house_number');
