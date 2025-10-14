1b6077be4526bceb036b10e0244a44b263857077
Asim Riaz <asimriazasim107@gmail.com>
2025-10-14 23:18:28 +0500

    subscript plan fixing

--- resources/views/providerdetail/all-plan.blade.php
+++ resources/views/providerdetail/all-plan.blade.php
@@ -272,8 +272,26 @@
             var planType = $('#plan_type').val();
             var planAmount = $('#plan_amount').text();
 
-            window.location.href =
-                `{{ route('stripe') }}?plan_id=${planId}&plan_type=${planType}&plan_amount=${planAmount}`;
+            $.ajax({
+                url: '{{ route('subscription.stripe.create') }}',
+                type: 'POST',
+                data: {
+                    _token: '{{ csrf_token() }}',
+                    plan_id: planId,
+                    plan_type: planType,
+                    plan_amount: planAmount
+                },
+                success: function(response) {
+                    if (response.status && response.url) {
+                        window.location.href = response.url;
+                    } else {
+                        alert('Stripe payment failed: ' + response.message);
+                    }
+                },
+                error: function(error) {
+                    alert('Stripe payment failed. Please try again.');
+                }
+            });
         });
 
         // Handle PayPal payment selection
