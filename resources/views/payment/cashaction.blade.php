acf93b2981fe0e6e6959cc79ede8136e0959baee
Asim Riaz <asimriazasim107@gmail.com>
2025-10-18 20:49:55 +0500

    aded somethig for desing

--- resources/views/payment/cashaction.blade.php
+++ resources/views/payment/cashaction.blade.php
@@ -14,16 +14,6 @@ $auth_user= authSession();
             </a>
         @endif
 
-        @if(auth()->user()->hasAnyRole(['admin']))
-            <a class="ml-6" href="{{ route('payment.destroy', $payment->id) }}" data--submit="payment{{$payment->id}}" 
-                data--confirmation='true' 
-                data--ajax="true"
-                data-datatable="reload"
-                data-title="{{ __('messages.delete_form_title',['form'=>  __('messages.payment') ]) }}"
-                title="{{ __('messages.delete_form_title',['form'=>  __('messages.payment') ]) }}"
-                data-message='{{ __("messages.delete_msg") }}'>
-                <i class="far fa-trash-alt text-danger"></i>
-            </a>
-        @endif
+        
     </div>
 {{ html()->form()->close() }}
