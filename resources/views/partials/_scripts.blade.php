6634f523febbb649a8bf797521414dc2f7091981
Asim Riaz <asimriazasim107@gmail.com>
2025-10-20 22:42:36 +0500

    -script

--- resources/views/partials/_scripts.blade.php
+++ resources/views/partials/_scripts.blade.php
@@ -1,6 +1,10 @@
  <!-- Backend Bundle JavaScript -->
  <script src="{{ asset('js/backend-bundle.min.js')}}"></script>
- 
+<script src="{{ asset('vendor/tinymce/js/tinymce/tinymce.min.js') }}"></script>
+<script src="{{ asset('vendor/tinymce/js/tinymce/jquery.tinymce.min.js') }}"></script>
+<link href="{{ asset('css/dragula.css') }}" rel="stylesheet">
+<script src="{{ asset('js/dragula.min.js') }}"></script>
+<script src="{{ asset('js/swiper-bundle.min.js') }}"></script>
 
 
 <script>
