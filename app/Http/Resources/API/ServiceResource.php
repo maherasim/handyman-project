82723f0234067655b4643ffa41e8b2e0d402ba67
Asim Riaz <asimriazasim107@gmail.com>
2025-11-02 14:31:26 +0500

    aded somethig phans

--- app/Http/Resources/API/ServiceResource.php
+++ app/Http/Resources/API/ServiceResource.php
@@ -72,7 +72,7 @@ class ServiceResource extends JsonResource
             'advance_payment_amount' => $this->is_enable_advance_payment 
                 ? ($this->advance_payment_amount === null ? 0 : (double) $this->advance_payment_amount) 
                 : (double) $advancePaymentPercentage,
-            'completed_booking_count' => $this->completed_booking_count ?? 2,
+            'completed_booking_count' => $this->completed_booking_count ?? Booking::where('service_id', $this->id)->where('status', 'completed')->count(),
             'total_views' => (int) ($this->total_views ?? 0),
         ];
     }
