bba9ccf26769246527497bf7049455f678156e29
Asim Riaz <asimriazasim107@gmail.com>
2025-12-30 22:38:15 +0500

    added MFA

--- app/Http/Resources/API/BankResource.php
+++ app/Http/Resources/API/BankResource.php
@@ -16,15 +16,21 @@ class BankResource extends JsonResource
     {
         return [
             'id'            => $this->id,
-            'provider_id'          => $this->provider_id,
-            'bank_name'        => $this->bank_name,
-            'branch_name'   => $this->branch_name,
+            'provider_id'  => $this->provider_id,
+            'bank_name'    => $this->bank_name,
+            'branch_name'  => $this->branch_name,
             'account_no'   => $this->account_no,
-            'ifsc_no'         => $this->ifsc_no,
-            'mobile_no'         => $this->mobile_no,
-            'aadhar_no'         => $this->aadhar_no,
-            'pan_no'         => $this->pan_no,
-            'is_default'   =>$this->is_default,
+            'account_holder' => $this->account_holder,
+            'mobile_no'    => $this->mobile_no,
+            'iban_no'      => $this->iban_no,
+            'bic_number'   => $this->bic_number,
+            'ifsc_no'      => $this->ifsc_no,
+            'aadhar_no'    => $this->aadhar_no,
+            'pan_no'       => $this->pan_no,
+            'status'       => $this->status,
+            'stripe_account' => $this->stripe_account,
+            'is_default'   => $this->is_default,
+            'bank_attachment' => getSingleMedia($this, 'bank_attachment', null),
         ];
     }
 }
