d7e3bdaae8d2009fc901ee5a136f0e1e781d54f3
maherasim <asimriazasim107@gmail.com>
2026-02-02 11:43:55 +0500

    added testing  code

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -257,7 +257,7 @@ class DashboardController extends Controller
         $handyman = UserResource::collection(User::myUsers()->where('status', 1)->take(4)->get());
 
         $total_revenue    = ProviderPayout::where('provider_id',$provider->id)
-            ->where('status','paid')
+            ->whereIn('status', ['paid', 'advance_paid'])
             ->sum('amount') ?? 0;
 
         $handymanIds = User::with('providerHandyman')
@@ -286,7 +286,7 @@ class DashboardController extends Controller
         $revenuedata = ProviderPayout::selectRaw('sum(amount) as total , DATE_FORMAT(updated_at , "%m") as month')
                 ->where('provider_id', $user->id)
                 ->whereYear('updated_at', date('Y'))
-                ->where('status', 'paid')
+                ->whereIn('status', ['paid', 'advance_paid'])
                 // ->whereIn('commission_status', ['paid'])
                 ->groupBy('month');
         $revenuedata= $revenuedata->get();
