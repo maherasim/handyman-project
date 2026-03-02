21316c068f795ecd7f08636c20459a615a060f97
maherasim <asimriazasim107@gmail.com>
2026-03-02 20:15:58 +0500

    added suppleir registration task

--- resources/views/postrequest/show.blade.php
+++ resources/views/postrequest/show.blade.php
@@ -205,7 +205,7 @@
         <button type="button" class="btn btn-warning rounded-pill px-4 py-2 shadow-sm me-2 d-inline-flex align-items-center gap-2 mt-2"
                 id="postbid-rate-now-btn" data-id="{{ $bid->id }}" data-bs-toggle="modal" data-bs-target="#postBidRatingModal">
             <i class="las la-star"></i>
-            <span>Rate Now</span>
+            <span>Rate Employer</span>
         </button>
     @endif
     @if(auth()->user()->user_type === 'user' && (int)auth()->id() === (int)($bid->customer_id ?? 0) && ($customerHasRatedProvider ?? false) && in_array(strtolower((string)($bid->status ?? '')), ['remaining_paid', 'confirm_done', 'completed']))
