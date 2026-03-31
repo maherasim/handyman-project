4615a5dbf1ffcc92225af746ec5c78d89df23a12
maherasim <asimriazasim107@gmail.com>
2026-03-31 16:10:00 +0500

    buyer qualification

--- resources/views/partials/_body_sidebar.blade.php
+++ resources/views/partials/_body_sidebar.blade.php
@@ -367,28 +367,29 @@
                     ->link->attr(['class' => '']);
             }
         }
-        $menu
-            ->add(
-                '<span>' .
-                    __('messages.handymen') .
-                    '</span><span class="custom-tooltip"><span class="tooltip-text">' .
-                    __('messages.handymen') .
-                    '</span></span>',
-                ['class' => ''],
-            )
-            ->prepend(
-                '<svg width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        if (auth()->user()->user_type !== 'handyman') {
+            $menu
+                ->add(
+                    '<span>' .
+                        __('messages.handymen') .
+                        '</span><span class="custom-tooltip"><span class="tooltip-text">' .
+                        __('messages.handymen') .
+                        '</span></span>',
+                    ['class' => ''],
+                )
+                ->prepend(
+                    '<svg width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <circle cx="12" cy="6" r="4" stroke="currentColor" stroke-width="1.5"/>
 <circle cx="17" cy="18" r="4" stroke="currentColor" stroke-width="1.5"/>
 <path d="M17 16.667V19.3337" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M15.6665 18L18.3332 18" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M14 20.8344C13.3663 20.9421 12.695 21 12 21C8.13401 21 5 19.2091 5 17C5 14.7909 8.13401 13 12 13C13.7135 13 15.2832 13.3518 16.5 13.9359" stroke="currentColor" stroke-width="1.5"/>
 </svg>',
-            )
-            ->nickname('handyman')
-            ->data('permission', 'handyman list')
-            ->link->attr(['class' => ''])
-            ->href('#handyman');
+                )
+                ->nickname('handyman')
+                ->data('permission', 'handyman list')
+                ->link->attr(['class' => ''])
+                ->href('#handyman');
 
         $menu->handyman
             ->add('<span>' . __('messages.list_form_title', ['form' => __('messages.handyman')]) . '</span>', [
@@ -485,6 +486,7 @@
 </svg>',
                 )
                 ->link->attr(['class' => '']);
+        }
         }
 
         if (auth()->user()->user_type !== 'handyman') {
