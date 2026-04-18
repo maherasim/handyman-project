3ccdf34b5b63ea0c643a73f312866bd015ce5d73
maherasim <asimriazasim107@gmail.com>
2026-04-18 10:45:44 +0500

    dashabord issue fixd

--- resources/views/booking/info.blade.php
+++ resources/views/booking/info.blade.php
@@ -109,7 +109,7 @@
             $grandTotal = $totalWithExtras + $taxAmount;
         @endphp
         <tr>
-            <td>{{ __('messages.grand_total') }}</td>
+            <td>{{ __('messages.grand_total') }} good</td>
             <td class="bk-value">{{ getPriceFormat($grandTotal) }}</td>
         </tr>
 
